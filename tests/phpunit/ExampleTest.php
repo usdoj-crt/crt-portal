5fc0a4bf512c04c29369c11089701cde3f173d89
Alex Soble <alex.soble@gsa.gov>
2019-07-12 10:29:31 -0500

    Created BLT project.

---
+++ tests/phpunit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace My\Example\Project\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ExampleTest.
+ */
+class ExampleTest extends TestCase {
+
+  /**
+   * Asserts that universal laws are constant.
+   */
+  public function testExample() {
+    $this->assertEquals(1, 1, "One equals one!");
+  }
+
+}
