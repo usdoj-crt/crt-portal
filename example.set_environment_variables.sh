c4f1d44c150fde313f2520b6906d6ef91c5dc27f
Alex Soble <alex.soble@gsa.gov>
2019-07-22 12:10:31 -0500

    Tweak example.set_environment_variables.sh

--- example.set_environment_variables.sh
+++ example.set_environment_variables.sh
@@ -1,16 +1,12 @@
-# All of the configuration below should be available from
-# the Acquia Cloud platform. Please see the below resources:
-
-# Acquia Cloud drush config:
+# All of the configuration below should be available from the Acquia Cloud platform. Please see the below resources:
 #
+# Acquia Cloud drush config:
 # * https://docs.acquia.com/blt/developer/drush/
 #
 # "Obtaining your subscription’s application ID":
-#
 # * https://docs.acquia.com/acquia-cloud/manage/applications/#obtaining-your-subscription-s-application-id
 #
 # Acquia Cloud Remote git:
-#
 # * Available from the Acquia Cloud dashboard.
 
 export ACQUIA_CLOUD_REMOTE_GIT=""
