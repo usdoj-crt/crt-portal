54f957e012a210afddd4cfa0ef251712e5348f31
Alex Soble <alex.soble@gsa.gov>
2019-07-22 11:57:30 -0500

    Add example.set_environment_variables.sh

---
+++ example.set_environment_variables.sh
@@ -0,0 +1,13 @@
+export ACQUIA_CLOUD_REMOTE_GIT=""
+
+export ACQUIA_APP_ID=""
+
+# Drush settings for dev environment: drush/sites/dojportal-blt.site.yml
+export ACQUIA_DEV_SSH_HOST=""
+export ACQUIA_DEV_URI=""
+export ACQUIA_DEV_DRUSH_USER=""
+
+# Drush settings for test environment: drush/sites/dojportal-blt.site.yml
+export ACQUIA_TEST_SSH_HOST=""
+export ACQUIA_TEST_URI=""
+export ACQUIA_TEST_DRUSH_USER=""
