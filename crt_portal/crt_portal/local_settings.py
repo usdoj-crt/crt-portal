2ed6f9a2a3f91f9ca34403b29ffba364c9a25145
lindsayyoung <lindsayn.young@gsa.gov>
2019-08-08 13:31:50 -0400

    Got docker running localy, need to add the NPM commands

--- crt_portal/crt_portal/local_settings.py
+++ crt_portal/crt_portal/local_settings.py
@@ -3,12 +3,11 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
 
