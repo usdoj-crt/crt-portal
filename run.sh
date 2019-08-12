374ef35646601a0f1b29a8d46e7581b891b8f946
Alex Soble <alex.soble@gsa.gov>
2019-08-12 15:33:29 -0500

    Remove duplicate npm install commands from run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,6 @@
 echo migrate database...
 python /code/crt_portal/manage.py migrate
 
-echo USWDS gulp commands...
-# running USWDS https://github.com/uswds/uswds-gulp
-npm install autoprefixer css-mqpacker cssnano gulp@^4.0.0 gulp-notify gulp-postcss gulp-rename gulp-replace gulp-sass gulp-sourcemaps path uswds@latest uswds-gulp@github:uswds/uswds-gulp --save-dev
-
 echo Starting Django Server…
 python /code/crt_portal/manage.py compress
 python /code/crt_portal/manage.py runserver 0.0.0.0:8000
