95fe6e949e22c9d965f567c9110e6ebcf4633e6d
lindsayyoung <lindsayn.young@gsa.gov>
2019-08-12 13:34:17 -0400

    Starting to implement more complex models, still need to work on the form.py

--- crt_portal/cts_forms/forms.py
+++ crt_portal/cts_forms/forms.py
@@ -1,12 +1,37 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, RadioSelect, Select, ModelMultipleChoiceField
 
-from .models import Report
+from .models import Report, ProtectedClass, StatesAndTerritories
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class WhatHappened(ModelForm):
     class Meta:
         model = Report
+        protected_class = ModelMultipleChoiceField(queryset=ProtectedClass.objects.all())
         fields = ['primary_complaint', 'protected_class']
+        widgets = {
+            'primary_complaint': RadioSelect,
+        }
+        logger.error('protected_class')
+
+
+    def __init__(self, *args, **kwargs):
+        # Only in case we build the form from an instance
+        if kwargs.get('instance'):
+            # We get the 'initial' keyword argument or initialize it
+            # as a dict if it didn't exist.
+            initial = kwargs.setdefault('initial', {})
+            # The widget for a ModelMultipleChoiceField expects
+            # a list of primary key for the selected data.
+            initial['protected_class'] = [t.pk for t in kwargs[instance].protected_class_set.all()]
+            logger.error(initial['protected_class'])
+            logger.error('hola')
+
+        ModelForm.__init__(self, *args, **kwargs)
+
+
 
 
 class Where(ModelForm):
--- crt_portal/cts_forms/models.py
+++ crt_portal/cts_forms/models.py
@@ -9,11 +9,25 @@ from .model_variables import *
 class InternalHistory(models.Model):
     note = models.CharField(max_length=500, null=False, blank=False,)
     create_date = models.DateTimeField(auto_now_add=True)
+    # add author
+
+class ProtectedClass(models.Model):
+    protected_class = models.CharField(max_length=100, null=True, blank=True, choices=PROTECTED_CLASS_CHOICES)
+
+    def __str__(self):
+        return self.protected_class
+
+
+class StatesAndTerritories(models.Model):
+    state_territory = models.CharField(max_length=100, null=True, blank=True, choices=STATES_AND_TERRITORIES)
+
+    def __str__(self):
+        return self.state_territory
 
 
 class Report(models.Model):
-    primary_complaint = models.CharField(max_length=100, choices=PRIMARY_COMPLAINT_CHOICES)
-    protected_class = models.CharField(max_length=100, null=True, blank=True, choices=PROTECTED_CLASS_CHOICES)
+    primary_complaint = models.CharField(max_length=100, choices=PRIMARY_COMPLAINT_CHOICES, default=None)
+    protected_class = models.ManyToManyField(ProtectedClass, blank=True,)
     place = models.CharField(max_length=100, null=True, blank=True, choices=PLACE_CHOICES)
     public_or_private_employer = models.CharField(max_length=100, null=True, blank=True, choices=PUBLIC_OR_PRIVATE_EMPLOYER_CHOICES)
     # location_details to come after user testing
@@ -25,7 +39,7 @@ class Report(models.Model):
     respondent_contact_ask = models.BooleanField(null=True)
     respondent_name = models.CharField(max_length=225, null=True, blank=True)
     respondent_city = models.CharField(max_length=700, null=True, blank=True)
-    respondent_state =  models.CharField(max_length=100, null=True, blank=True, choices=STATES_AND_TERRITORIES)
+    respondent_state =  models.ManyToManyField(StatesAndTerritories, blank=True, related_name='respondent_state')
     violation_summary = models.TextField()
     when = models.CharField(max_length=700, null=True, blank=True, choices=WHEN_CHOICES)
     how_many = models.CharField(max_length=700, null=True, blank=True, choices=HOW_MANY_CHOICES)
@@ -35,7 +49,7 @@ class Report(models.Model):
     contact_given_name = models.CharField(max_length=225, null=True, blank=True)
     contact_family_name = models.CharField(max_length=225, null=True, blank=True)
     contact_email = models.EmailField(null=True, blank=True)
-    contact_state = models.CharField(max_length=100, null=True, blank=True, choices=STATES_AND_TERRITORIES)
+    contact_state = models.ManyToManyField(StatesAndTerritories, blank=True, related_name='contact_state')
     contact_address_line_1 = models.CharField(max_length=225, null=True, blank=True)
     contact_address_line_2 = models.CharField(max_length=225, null=True, blank=True)
     # TODO, upgrade to add validation https://pypi.org/project/django-phone-field/
