f05be16c1eb375d5ba0d0325ecac9a20b83dd203
lindsayyoung <lindsayn.young@gsa.gov>
2019-08-16 16:19:22 -0400

    working on tests

--- crt_portal/cts_forms/tests.py
+++ crt_portal/cts_forms/tests.py
@@ -4,25 +4,15 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import ViolationReport
+from .models import ProtectedClass, Report
 
 
-def create_question(description_text, days):
-    """
-    Create a question with the given `description_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return ViolationReport.objects.create(description_text=description_text, create_date=time)
+class MyTests(TestCase):
+    # all tests must start with the word test
+    def test_has_required_fields(self):
+        response = self.client.post("/report/", {'primary_complaint': 'vote', 'place': 'Null', 'public_or_private_employer': 'Null', 'employer_size': 'Null', 'public_or_private_school': 'Null', 'public_or_private_facility': 'Null', 'public_or_private_healthcare': 'Null', 'respondent_contact_ask': 'Null', 'respondent_type': 'Null', 'respondent_name': 'Null', 'respondent_city': 'Null', 'respondent_state': 'Null', 'violation_summary': 'Hello!', 'when': 'Null', 'how_many': 'Null', 'who_reporting_for': 'Null', 'relationship': 'Null', 'do_not_contact': 'Null', 'contact_given_name': 'Null', 'contact_family_name': 'Null', 'contact_email': 'Null', 'contact_state': 'Null', 'contact_address_line_1': 'Null', 'contact_address_line_2': 'Null', 'contact_phone': 'Null', 'protected_class': '<QuerySet []>'})
+        print(response)
+        self.assertFormError(response, 'form', 'something', 'This field is required.')
 
-class QuestionDetailViewTests(TestCase):
-    def test_past_question(self):
-        """
-        The detail view of a question with a pub_date in the past
-        displays the question's text.
-        """
-        past_question = create_question(description_text='Past Question.', days=-5)
-        url = reverse('crt_forms:detail', args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.description_text)
+    # primary complaint
+    # summary
