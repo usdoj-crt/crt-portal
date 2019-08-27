785a7207fb46c0d53f0aa9c9e2eced4f78d85624
Alex Soble <alex.soble@gsa.gov>
2019-08-27 13:10:12 -0500

    Revert local settings to own file and add noqa as per @lindsayyoung suggestion

--- crt_portal/crt_portal/settings.py
+++ crt_portal/crt_portal/settings.py
@@ -186,16 +186,4 @@ COMPRESS_PRECOMPILERS = (
 COMPRESS_OFFLINE = True
 
 if environment == 'LOCAL':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'HOST': 'db',  # set in docker-compose.yml
-            'PORT': 5432  # default postgres port
-        }
-    }
-
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    ALLOWED_HOSTS = ['localhost', '0.0.0.0']
-    DEBUG = True
+    from .local_settings import *  # noqa: F401,F403
