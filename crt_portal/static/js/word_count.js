268149bd5468d0f2b3ad18341c2fe57c56d34d2c
Alex Soble <alex.soble@gsa.gov>
2019-10-01 15:05:29 -0500

    JS refactor

--- crt_portal/static/js/word_count.js
+++ crt_portal/static/js/word_count.js
@@ -1,17 +1,29 @@
-if (document.getElementById("id_1-violation_summary")) {
-  document.getElementById("id_1-violation_summary").onkeyup = function () {
-      document.getElementById("display_count").innerHTML =  (500 - this.value.match(/\S+/g).length);
-      var words = this.value.match(/\S+/g).length;
-      if (words > 500) {
-        // Split the string on first 500 words and rejoin on spaces
-        var trimmed = this.value.split(/\s+/, 500).join(" ");
-        // replace the input with trimmed text
-        document.getElementById("id_1-violation_summary").value = trimmed;
-        document.getElementById("count_message").innerHTML = ' word limit reached';
-        document.getElementById("display_count").innerHTML = '500';
-      } else {
-        document.getElementById("display_count").innerHTML =  (500 - this.value.match(/\S+/g).length);
-        document.getElementById("count_message").innerHTML = ' word(s) remaining'
-      }
-  };
+var textAreaElem = document.getElementById("id_1-violation_summary");
+var displayCountElem = document.getElementById("display_count");
+var countMessageElem = document.getElementById("count_message");
+
+function updateWordCount (event) {
+    var value = event ? event.target.value : '';
+    var words = value ? value.match(/\S+/g).length : 0;
+
+    displayCountElem.innerHTML = (500 - words);
+
+    if (words > 500) {
+      // Split the string on first 500 words and rejoin on spaces
+      var trimmed = textAreaElem.value.split(/\s+/, 500).join(" ");
+      // replace the input with trimmed text
+      textAreaElem.value = trimmed;
+      countMessageElem.innerHTML = ' word limit reached';
+      displayCountElem.innerHTML = '500';
+    } else {
+      displayCountElem.innerHTML =  (500 - words);
+      countMessageElem.innerHTML = ' word(s) remaining'
+    }
 };
+
+if (textAreaElem) {
+  updateWordCount();
+
+  document.addEventListener('keyup', updateWordCount);
+};
+
