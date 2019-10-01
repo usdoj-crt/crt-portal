287b5d58f4a28e9e802359075764c9b7506b95a0
Alex Soble <alex.soble@gsa.gov>
2019-10-01 16:48:13 -0500

    Make word counter work with Back button

--- crt_portal/static/js/word_count.js
+++ crt_portal/static/js/word_count.js
@@ -2,8 +2,11 @@ var textAreaElem = document.getElementById("id_1-violation_summary");
 var displayCountElem = document.getElementById("display_count");
 var countMessageElem = document.getElementById("count_message");
 
-function updateWordCount (event) {
-    var value = event ? event.target.value : '';
+function updateWordCount (e) {
+    // Ignore `e` and read the value directly from the textarea here;
+    // we want this function to work even if the user hasn't typed
+    // anything (e.g. if textarea has initial content).
+    var value = textAreaElem.value;
     var words = value ? value.match(/\S+/g).length : 0;
 
     displayCountElem.innerHTML = (500 - words);
@@ -22,6 +25,9 @@ function updateWordCount (event) {
 };
 
 if (textAreaElem) {
+  // Fire `updateWordCount()` on page load because textarea may have
+  // some initial content; for example if the user fills in the textarea,
+  // presses "Next" and then uses the back button to navigate back.
   updateWordCount();
 
   document.addEventListener('keyup', updateWordCount);
