15b9206066fa81e9cdc58b3b1a2b35ae700a7c93
Alex Soble <alex.soble@gsa.gov>
2019-10-04 16:05:49 -0500

    Long string

--- crt_portal/cts_forms/forms.py
+++ crt_portal/cts_forms/forms.py
@@ -58,12 +58,7 @@ class Details(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
         self.fields['violation_summary'].widget.attrs['class'] = 'usa-textarea'
-        self.fields['violation_summary'].help_text = (
-            "This is your opportunity to share any additional details you think would be helpful "
-            "for us to know like time, names, witnesses to the event, or anything you haven’t "
-            "already stated. Document upload is not available at this time, but please describe "
-            "if you have evidence you’d like to include later."
-        )
+        self.fields['violation_summary'].help_text = "This is your opportunity to share any additional details you think would be helpful for us to know like time, names, witnesses to the event, or anything you haven’t already stated. Document upload is not available at this time, but please describe if you have evidence you’d like to include later."
 
     class Meta:
         model = Report
