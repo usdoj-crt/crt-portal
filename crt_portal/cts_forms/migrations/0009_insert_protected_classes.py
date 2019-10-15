fc07a7e72cc9ce7bc62fcdf6b0aca9ef364a9baf
lindsayyoung <lindsayn.young@gsa.gov>
2019-10-15 12:54:58 -0400

    Adding datamigration for protected class

---
+++ crt_portal/cts_forms/migrations/0009_insert_protected_classes.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+from cts_forms.models import ProtectedClass
+from cts_forms.model_variables import PROTECTED_CLASS_CHOICES
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cts_forms', '0008_auto_20191011_2101'),
+    ]
+
+    def retrieve_or_create_choices(*args, **defaults):
+        choices = []
+        for choice in PROTECTED_CLASS_CHOICES:
+            c = ProtectedClass.objects.get_or_create(protected_class=choice)
+            choices.append(c[0].pk)
+        return ProtectedClass.objects.filter(pk__in=choices)
+
+    operations = [
+        migrations.RunPython(retrieve_or_create_choices),
+    ]
