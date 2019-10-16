bea1cd0a8dc5d5160fc4216bb5250b1e59d6b13a
lindsayyoung <lindsayn.young@gsa.gov>
2019-10-16 16:47:36 -0400

    Adding the migrations for explicitly ordering the protected class choices

---
+++ crt_portal/cts_forms/migrations/0012_insert_protected_class_order.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+from cts_forms.models import ProtectedClass
+from cts_forms.model_variables import PROTECTED_CLASS_CHOICES
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cts_forms', '0011_protectedclass_form_order'),
+    ]
+
+    def load_class_order(*args, **defaults):
+        n = 0
+        for choice in PROTECTED_CLASS_CHOICES:
+            c = ProtectedClass.objects.get(protected_class=choice)
+            c.form_order = n
+            c.save()
+            n += 1
+
+    operations = [
+        migrations.RunPython(load_class_order),
+    ]
