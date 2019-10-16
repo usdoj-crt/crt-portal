e73f9308bd93ae188fa3dd826b74564ba3e47dde
Jacklynn <2560618+Jacklynn@users.noreply.github.com>
2019-10-16 10:52:45 -0700

    Update freeform heading to "details"
    Felt repetitive before, also more consistent with Contact page.

--- crt_portal/cts_forms/views.py
+++ crt_portal/cts_forms/views.py
@@ -45,7 +45,7 @@ class CRTReportWizard(SessionWizardView):
         # This title appears in large font above the question elements
         ordered_step_titles = [
             'Contact',
-            'Please provide details on what happened'
+            'Details'
         ]
         current_step_title = ordered_step_titles[int(self.steps.current)]
 
