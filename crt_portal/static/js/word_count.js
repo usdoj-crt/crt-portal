478e6562ee773fc0283d7609b0776a7a74890613
Alex Soble <alex.soble@gsa.gov>
2019-10-16 15:01:12 -0500

    Create a clearer screen reader word count message
    + Include both number of words left and the phrase "word count remaining"
    + Refactored some JS while I was touching the code

--- crt_portal/static/js/word_count.js
+++ crt_portal/static/js/word_count.js
@@ -1,44 +1,61 @@
 var textAreaElem = document.getElementById("id_1-violation_summary");
-var displayCountElem = document.getElementById("display_count");
-var countMessageElem = document.getElementById("count_message");
-var wordLimitAlert = document.getElementById("word-limit-alert");
+
+// "Words remaining" message
+var wordLimitMessage = document.getElementById("word_limit_message");
+
+// Word limit alert state
+var wordLimitAlert = document.getElementById("word_limit_alert");
+
+// Wraps both "words remaining" message and alert state
 var wordCountArea = document.getElementById("word_count_area");
 
-// Show word count area for JS-enabled browsers:
+// Show word count area for JS-enabled browsers
 if (wordCountArea) { wordCountArea.removeAttribute('hidden'); }
 
-function updateWordCount (e) {
+function onBelowLimit (wordCount) {
+  var wordsRemaining = String(500 - wordCount);
+  var description = (wordsRemaining === 1) ? ' word remaining' : ' words remaining';
+
+  // Unset alert states
+  wordLimitAlert.setAttribute('hidden', ''); // hide
+  textAreaElem.classList.remove('bg-gold-outline');
+  textAreaElem.setAttribute('aria-invalid', 'false');
+
+  // Update word counter
+  wordLimitMessage.removeAttribute('hidden');
+  wordLimitMessage.innerHTML = wordsRemaining + description;
+}
+
+function onEqualOrExceedLimit (wordCount) {
+  // Trim the text down to the first 500 words:
+  var trimmed = textAreaElem.value.split(" ", 500).join(" ")
+                                  .split("\t", 500).join("\t")
+                                  .split("\n", 500).join("\n");
+  textAreaElem.value = trimmed;
+
+  // Unset message
+  wordLimitMessage.setAttribute('hidden', ''); // hide
+
+  // Set alert state
+  textAreaElem.setAttribute('aria-invalid', 'true');
+  textAreaElem.classList.add('bg-gold-outline');
+  wordLimitAlert.removeAttribute('hidden');
+}
+
+function updateWordCountArea (e) {
     // Ignore `e` and read the value directly from the textarea here;
     // we want this function to work even if the user hasn't typed
     // anything (e.g. if textarea has initial content).
     var value = textAreaElem.value;
-    var words = value ? value.match(/\S+/g).length : 0;
+    var wordCount = value ? value.match(/\S+/g).length : 0;
 
-    displayCountElem.innerHTML = (500 - words);
-
-    if (words >= 500) {
-      // Word count greater than or equal to 500 word limit.
-      // Trim the text down to the first 500 words:
-      var trimmed = textAreaElem.value.split(" ", 500).join(" ")
-                                      .split("\t", 500).join("\t")
-                                      .split("\n", 500).join("\n");
-      textAreaElem.value = trimmed;
-
-      // Update display for user:
-      displayCountElem.innerHTML = '0';
-      textAreaElem.classList.add('bg-gold-outline');
-      wordLimitAlert.removeAttribute('hidden');
-      wordLimitAlert.setAttribute('role', 'alert');
+    if (wordCount >= 500) {
+      onEqualOrExceedLimit(wordCount);
     } else {
-      var wordsRemaining = 500 - words;
-      displayCountElem.innerHTML = wordsRemaining;
-      countMessageElem.value = (wordsRemaining === 1) ? 'word remaining' : 'words remaining';
-      textAreaElem.classList.remove('bg-gold-outline');
-      wordLimitAlert.setAttribute('hidden', '');
-      wordLimitAlert.removeAttribute('role');
+      onBelowLimit(wordCount);
     }
 
-    if (words >= 400) {
+    if (wordCount >= 400) {
       wordCountArea.setAttribute('aria-live', 'assertive');
     } else {
       wordCountArea.setAttribute('aria-live', 'polite');
@@ -49,7 +66,7 @@ if (textAreaElem) {
   // Fire `updateWordCount()` on page load because textarea may have
   // some initial content; for example if the user fills in the textarea,
   // presses "Next" and then uses the back button to navigate back.
-  updateWordCount();
+  updateWordCountArea();
 
-  document.addEventListener('keyup', updateWordCount);
+  document.addEventListener('keyup', updateWordCountArea);
 };
