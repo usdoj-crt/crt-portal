967085702768abeee59673a7081f6a12c46244ee
Alex Soble <alex.soble@gsa.gov>
2019-11-04 16:44:22 -0600

    Missing elem guard and code comment

--- crt_portal/static/js/focus_alert.js
+++ crt_portal/static/js/focus_alert.js
@@ -1,6 +1,11 @@
 function triggerAlert() {
+  // querySelector() will find the first element matching the
+  // CSS selector; the JS that follows will announce it.
   var alertEl = document.querySelector('.usa-alert__text');
-  alertEl.setAttribute('role', 'alert');
+
+  if (alertEl) {
+    alertEl.setAttribute('role', 'alert');
+  }
 }
 
 window.onload = triggerAlert();
