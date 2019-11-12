29cc32b459fcd430d2ae578c486d4e2a1788e905
lindsayyoung <lindsayn.young@gsa.gov>
2019-11-12 11:20:14 -0500

    Remove stray blank line

--- crt_portal/cts_forms/test_data.py
+++ crt_portal/cts_forms/test_data.py
@@ -6,4 +6,3 @@ SAMPLE_REPORT = {
     'contact_phone': "202-867-5309",
     'violation_summary': "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.",
 }
-
