55af3453897be782667c4d61da1aa15dbf6b7091
lindsayyoung <lindsayn.young@gsa.gov>
2019-11-12 11:15:55 -0500

    Adding tests for paging functionality and display

---
+++ crt_portal/cts_forms/test_data.py
@@ -0,0 +1,9 @@
+SAMPLE_REPORT = {
+    'other_class': "test other",
+    'contact_first_name': "Lincoln",
+    'contact_last_name': "Abraham",
+    'contact_email': "Lincoln@usa.gov",
+    'contact_phone': "202-867-5309",
+    'violation_summary': "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.",
+}
+
