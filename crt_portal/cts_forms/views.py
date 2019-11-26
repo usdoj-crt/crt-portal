53183a09854a5e51c45eea6fd23ad9b2c95319f6
Adam Biagianti <adam.biagianti@gmail.com>
2019-11-26 15:06:01 -0500

    Super basic URL route and view

--- crt_portal/cts_forms/views.py
+++ crt_portal/cts_forms/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 
 from formtools.wizard.views import SessionWizardView
 
+from django.core import serializers
+
 from .models import Report, ProtectedClass
 from .model_variables import PROTECTED_CLASS_CODES
 from .page_through import pagination
@@ -56,6 +58,13 @@ def IndexView(request):
 
     return render_to_response('forms/index.html', {'data_dict': data, 'page_format': page_format, 'page_args': page_args, 'sort_state': sort})
 
+@login_required
+def ShowView(request, id):
+    report = Report.objects.get(id=1)
+
+    return render_to_response('forms/report_viewer/show.html', {
+        'data': serializers.serialize('json', [ report, ])
+    })
 
 TEMPLATES = [
     # Contact
