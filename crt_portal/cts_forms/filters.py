6fe699f56ef0c850110c8f231b90374870613059
lindsayyoung <lindsayn.young@gsa.gov>
2019-12-12 16:25:49 -0500

    Adding an extra test for filtering

--- crt_portal/cts_forms/filters.py
+++ crt_portal/cts_forms/filters.py
@@ -4,10 +4,10 @@ from .models import Report
 
 # To add a new filter option for Reports, add the field name and expected filter behavior
 filter_options = {
-    'assigned_section': 'match',
-    'primary_complaint': 'match',
-    'status': 'match',
-    'location_state': 'match',
+    'assigned_section': '__in',
+    'primary_complaint': '__in',
+    'status': '__in',
+    'location_state': '__in',
 }
 
 # Populate query with valid filterable fields
@@ -15,12 +15,12 @@ def report_filter(request):
     kwargs = {}
     filters = {}
     for field in filter_options.keys():
-        if filter_options[field] == 'match':
-            filter_list = request.GET.getlist(field)
-            if len(filter_list) > 0:
+        filter_list = request.GET.getlist(field)
+        if len(filter_list) > 0:
+            filters[field] = request.GET.getlist(field)
+            if filter_options[field] == '__in':
                 # works for one or more options with exact matches
                 kwargs[f'{field}__in'] = request.GET.getlist(field)
-                filters[field] = request.GET.getlist(field)
 
     # returns a query and a dictionary that we can use to keep track of the filters we apply
     return Report.objects.filter(**kwargs), filters
