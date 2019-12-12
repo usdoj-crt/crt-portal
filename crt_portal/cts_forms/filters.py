47d0f20c489886a9a8a13770fa53de8189dfaa9f
lindsayyoung <lindsayn.young@gsa.gov>
2019-12-12 18:45:30 -0500

    adding text and date sort options
     - adds sort for text search using postgres's robust search capabilities
     - adds sort by min and max date

--- crt_portal/cts_forms/filters.py
+++ crt_portal/cts_forms/filters.py
@@ -1,5 +1,7 @@
 # Class to handle filtering Reports by supplied query params,
 # provided they are valid filterable model properties.
+import datetime
+
 from .models import Report
 
 # To add a new filter option for Reports, add the field name and expected filter behavior
@@ -8,6 +10,17 @@ filter_options = {
     'primary_complaint': '__in',
     'status': '__in',
     'location_state': '__in',
+    'primary_complaint': '__in',
+    'contact_first_name': '__search',
+    'contact_last_name': '__search',
+    'contact_email': '__search',
+    'other_class': '__search',
+    'violation_summary': '__search',
+    'location_name': '__search',
+    'location_address_line_1': '__search',
+    'location_address_line_2': '__search',
+    'create_date_start': '__gte',
+    'create_date_end': '__lte',
 }
 
 
@@ -22,6 +35,15 @@ def report_filter(request):
             if filter_options[field] == '__in':
                 # works for one or more options with exact matches
                 kwargs[f'{field}__in'] = request.GET.getlist(field)
+            elif filter_options[field] == '__search':
+                # takes one phrase
+                kwargs[f'{field}__search'] = request.GET.getlist(field)[0]
+            elif field.startswith('create_date'):
+                # filters by a start date or an end date expects ddmmyyyy
+                year = int(request.GET.getlist(field)[0][:4])
+                month = int(request.GET.getlist(field)[0][4:6])
+                day = int(request.GET.getlist(field)[0][6:])
+                kwargs[f'create_date{filter_options[field]}'] = datetime.date(year, month, day)
 
-    # returns a query and a dictionary that we can use to keep track of the filters we apply
-    return Report.objects.filter(**kwargs), filters
+    # returns keyword arguments that will be used for filtering, and a dictionary that we can use to keep track of the filters we apply
+    return kwargs, filters
