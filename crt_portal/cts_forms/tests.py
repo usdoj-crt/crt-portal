7257b22772aa1beae87b02d639ffe2850f12ca4a
imran lodi <imran.lodi@usdoj.gov>
2019-12-17 14:11:42 -0500

    fixed Flake8 error

--- crt_portal/cts_forms/tests.py
+++ crt_portal/cts_forms/tests.py
@@ -288,6 +288,7 @@ class Validation_Form_Tests(TestCase):
         self.assertFalse('hatecrimes_trafficking<ul class="errorlist"><li>' in str(form.errors))
         self.assertTrue(f'<ul class="errorlist"><li>{PRIMARY_COMPLAINT_ERROR}' in str(form.errors))
 
+
 class ContactValidationTests(TestCase):
     def test_non_ascii_name(self):
         form = Contact(data={
