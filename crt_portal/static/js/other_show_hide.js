c07dbd99d43dfbf636b68cf3a42a87a030d26ee7
lindsayyoung <lindsayn.young@gsa.gov>
2019-12-17 14:48:36 -0500

    Discribed the index for other and made it a constant

--- crt_portal/static/js/other_show_hide.js
+++ crt_portal/static/js/other_show_hide.js
@@ -1,11 +1,12 @@
 var otherbox = document.getElementById('other-class-option');
 otherbox.setAttribute('hidden', '');
 
-var otherCheck = document.querySelectorAll('.' + 'usa-checkbox').item(13);
-var otherCheckID = document.querySelectorAll('.' + 'usa-checkbox__input').item(13).id;
+var OtherIndex = 13
+var otherCheck = document.querySelectorAll('.' + 'usa-checkbox').item(OtherIndex);
+var otherCheckID = document.querySelectorAll('.' + 'usa-checkbox__input').item(OtherIndex).id;
 
 function checkOther(elem) {
-  var checkBox = document.querySelectorAll('.' + 'usa-checkbox__input').item(13);
+  var checkBox = document.querySelectorAll('.' + 'usa-checkbox__input').item(OtherIndex);
   var otherArea = document.getElementById('other-class-option');
   if (checkBox.checked == true) {
     otherArea.removeAttribute('hidden');
@@ -17,5 +18,5 @@ function checkOther(elem) {
 otherCheck.setAttribute('onclick', 'checkOther(otherCheckID);');
 
 window.selectionchange = checkOther(
-  document.querySelectorAll('.' + 'usa-checkbox__input').item(13).id
+  document.querySelectorAll('.' + 'usa-checkbox__input').item(OtherIndex).id
 );
