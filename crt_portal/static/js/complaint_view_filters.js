2bd912a998090a237d6692e6cb85413ce7a29d67
Adam Biagianti <adam.biagianti@gmail.com>
2020-01-07 07:41:00 -0800

    Fix bugs in filtering
    * Add location_city_town key to data model
    * Wrap model values in array before generating query params
      to ensure a consistent boolean check for the existence of a value,
      and to eliminate sparsely populated arrays output from the makeQueryParams fn

--- crt_portal/static/js/complaint_view_filters.js
+++ crt_portal/static/js/complaint_view_filters.js
@@ -65,12 +65,12 @@
 
     return keys.reduce(function(memo, key) {
       var paramValue = params[key];
+      var valueAsList = paramValue instanceof Array ? paramValue : [paramValue];
 
-      if (!paramValue) {
+      if (!paramValue.length) {
         return memo;
       }
 
-      var valueAsList = paramValue instanceof Array ? paramValue : [paramValue];
       var paramsString = valueAsList
         .reduce(function(accum, value) {
           accum.push(makeQueryParam(key, value));
@@ -123,6 +123,7 @@
     location_name: '',
     location_address_line_1: '',
     location_address_line_2: '',
+    location_city_town: '',
     create_date_start: '',
     create_date_end: '',
     sort: '',
