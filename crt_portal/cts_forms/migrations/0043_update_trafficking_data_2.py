4200f22b035fb040946b4c5de70d9de21c7efc48
lindsayyoung <lindsayn.young@gsa.gov>
2020-02-20 15:44:09 -0500

    There won't be the old version if this is migration is running for the first time, like in a test

--- crt_portal/cts_forms/migrations/0043_update_trafficking_data_2.py
+++ crt_portal/cts_forms/migrations/0043_update_trafficking_data_2.py
@@ -2,10 +2,6 @@ from django.db import migrations, models
 from cts_forms.models import HateCrimesandTrafficking
 from cts_forms.model_variables import HATE_CRIMES_TRAFFICKING_MODEL_CHOICES
 
-import logging
-
-logger = logging.getLogger(__name__)
-
 
 class Migration(migrations.Migration):
 
@@ -14,9 +10,12 @@ class Migration(migrations.Migration):
     ]
 
     def load_new_text(*args, **defaults):
-        hc_object = HateCrimesandTrafficking.objects.get(hatecrimes_trafficking_option='Coerced or forced to do work or perform a commercial sex act')
-        hc_object.hatecrimes_trafficking_option = 'Coerced or forced to do work or perform a sex act in exchange for something of value'
-        hc_object.save()
+        try:
+            hc_object = HateCrimesandTrafficking.objects.get(hatecrimes_trafficking_option='Coerced or forced to do work or perform a commercial sex act')
+            hc_object.hatecrimes_trafficking_option = 'Coerced or forced to do work or perform a sex act in exchange for something of value'
+            hc_object.save()
+        except HateCrimesandTrafficking.DoesNotExist:
+            pass
 
     operations = [
         migrations.RunPython(load_new_text),
