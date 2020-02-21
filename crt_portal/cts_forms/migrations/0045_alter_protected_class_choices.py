52703fadac9702911c489de6483caf76206058c8
lindsayyoung <lindsayn.young@gsa.gov>
2020-02-21 15:03:32 -0500

    The new migrations won't have the reanames

--- crt_portal/cts_forms/migrations/0045_alter_protected_class_choices.py
+++ crt_portal/cts_forms/migrations/0045_alter_protected_class_choices.py
@@ -11,21 +11,24 @@ class Migration(migrations.Migration):
     ]
 
     def retrieve_or_create_choices(*args, **defaults):
-        # rename an existing objects
-        g = ProtectedClass.objects.get(protected_class='Sex or gender identity (including gender stereotypes) or pregnancy')
-        g.protected_class = 'Gender identity (including gender stereotypes)'
-        g.save()
-        f = ProtectedClass.objects.get(protected_class='Family, marriage, or parental status')
-        f.protected_class = 'Family, marital, or parental status'
-        f.save()
-        g = ProtectedClass.objects.get(protected_class='Genetic information')
-        g.protected_class = 'Genetic information (including family medical history)'
-        g.save()
-        # remove form order from old object
-        m = ProtectedClass.objects.get(protected_class='Military status')
-        m.form_order = None
-        m.code = 'military'
-        m.save()
+        try:
+            # rename an existing objects
+            g = ProtectedClass.objects.get(protected_class='Sex or gender identity (including gender stereotypes) or pregnancy')
+            g.protected_class = 'Gender identity (including gender stereotypes)'
+            g.save()
+            f = ProtectedClass.objects.get(protected_class='Family, marriage, or parental status')
+            f.protected_class = 'Family, marital, or parental status'
+            f.save()
+            g = ProtectedClass.objects.get(protected_class='Genetic information')
+            g.protected_class = 'Genetic information (including family medical history)'
+            g.save()
+            # remove form order from old object
+            m = ProtectedClass.objects.get(protected_class='Military status')
+            m.form_order = None
+            m.code = 'military'
+            m.save()
+        except ProtectedClass.DoesNotExist:
+            pass
 
         # Add code and new form order to all objects
         for field in PROTECTED_CLASS_FIELDS:
