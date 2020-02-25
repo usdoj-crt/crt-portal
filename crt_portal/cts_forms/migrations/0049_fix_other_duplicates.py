881aeb978f4b24e1cadf97620cf636d72af853cd
lindsayyoung <lindsayn.young@gsa.gov>
2020-02-25 12:32:11 -0500

    Migrations for other work

---
+++ crt_portal/cts_forms/migrations/0049_fix_other_duplicates.py
@@ -0,0 +1,40 @@
+from django.db import migrations, models
+
+from cts_forms.models import Report, ProtectedClass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cts_forms', '0046_update_primary_complaint'),
+    ]
+
+    def retrieve_or_create_choices(*args, **defaults):
+        # remove other codes from "other" code from "Other", since codes are unique
+        old_other = ProtectedClass.objects.get(code='other')
+        old_other.code = 'old'
+        old_other.save()
+
+        # label data should be attributed to
+        real_other = ProtectedClass(
+            protected_class='Other reason',
+            form_order=14,
+            code='other'
+        )
+        real_other.save()
+        # pull records that need to be updated
+        update_records = Report.objects.filter(protected_class__protected_class__in=['other', 'Other'])
+        # loop through the records to add the correct relationship
+        for record in update_records:
+            record.protected_class.add(real_other)
+
+        # remove the incorrect "other" variants
+        ProtectedClass.objects.get(protected_class='Other').delete()
+        try:
+            ProtectedClass.objects.get(protected_class='other').delete()
+        except ProtectedClass.DoesNotExist:
+            pass
+
+    operations = [
+        migrations.RunPython(retrieve_or_create_choices),
+    ]
