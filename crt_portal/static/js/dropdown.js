86daae3fa242536ba88e789ec362eacba37aca8f
Adam Biagianti <adam.biagianti@gmail.com>
2020-02-27 09:43:08 -0800

    Clarify styles, add filter partial, JS improvement
    * Removes unused styles (filters.scss)
    * Adds filter html template that individual filter components can
      extend. Filter.html uses new crt-dropdown component
    * Adds JS to handle dropdown behavior. Similar to uswds accordion,
      but not tied to the uswds accordion classes
    * Minor tweaks to the filter JS to allow us to clear filters
      Now, instead of directly mutating the initial filter state,
      we copy the initial state and mutate the copy. This allows us
      to restore the filter model to its defaults before sending the new query
      to the backend

--- crt_portal/static/js/dropdown.js
+++ crt_portal/static/js/dropdown.js
@@ -1,4 +1,4 @@
-(function(root) {
+(function() {
   function Dropdown(el) {
     var el = el;
     var control = el.querySelector('.title');
@@ -25,20 +25,39 @@
       get control() {
         return control;
       },
+      get isVisible() {
+        return isVisible;
+      },
       toggle(event) {
         if (event.target !== control) {
           return;
         }
 
         _toggle();
+      },
+      hide() {
+        isVisible = false;
+        content.setAttribute('hidden', true);
+        control.setAttribute('aria-expanded', isVisible);
       }
     };
   }
 
-  var dropdowns = Array.prototype.slice.call(document.querySelectorAll('[data-crt-dropdown]'));
+  var dropdownNodes = Array.prototype.slice.call(document.querySelectorAll('[data-crt-dropdown]'));
 
-  dropdowns.forEach(function(node) {
+  var dropdowns = dropdownNodes.map(function(node) {
     var dropdown = Dropdown(node);
-    dropdown.control.addEventListener('click', dropdown.toggle);
+    return dropdown;
+  });
+
+  dropdowns.forEach(function(dropdown) {
+    dropdown.control.addEventListener('click', function(event) {
+      dropdowns
+        .filter(function(dropdown) {
+          return dropdown.control !== event.target;
+        })
+        .forEach(function(d) { d.hide() });
+      dropdown.toggle(event);
+    });
   });
 })(window);
