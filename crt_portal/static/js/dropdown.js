f07c1cc4fd6f2b2c0018edabf9e3db2e1323c582
Adam Biagianti <adam.biagianti@gmail.com>
2020-02-27 15:07:23 -0800

    Dropodown logic handles edge cases
    * Not super happy with the amount of logic in if statements

--- crt_portal/static/js/dropdown.js
+++ crt_portal/static/js/dropdown.js
@@ -1,24 +1,13 @@
 (function() {
+  /**
+   * Controls the visibility of a dropdown component
+   * @param {HTMLElement} el Node that represents the dropdown
+   */
   function Dropdown(el) {
     var el = el;
     var control = el.querySelector('.title');
     var content = el.querySelector('.content');
     var isVisible = true;
-
-    function _toggle() {
-      control.setAttribute('aria-expanded', isVisible);
-
-      if (isVisible) {
-        content.setAttribute('hidden', true);
-        el.classList.remove('expanded');
-      } else {
-        el.classList.add('expanded');
-        content.removeAttribute('hidden');
-      }
-      isVisible = !isVisible;
-    }
-
-    _toggle();
 
     return {
       get el() {
@@ -30,43 +19,103 @@
       get isVisible() {
         return isVisible;
       },
-      toggle(event) {
-        if (event.target !== control) {
-          return;
-        }
-
-        _toggle();
-      },
       hide() {
         isVisible = false;
-        content.setAttribute('hidden', true);
+        content.setAttribute('hidden', !isVisible);
         control.setAttribute('aria-expanded', isVisible);
         el.classList.remove('expanded');
+      },
+      show() {
+        isVisible = true;
+        content.removeAttribute('hidden');
+        control.setAttribute('aria-expanded', isVisible);
+        el.classList.add('expanded');
       }
     };
   }
 
   var dropdownNodes = Array.prototype.slice.call(document.querySelectorAll('[data-crt-dropdown]'));
-
   var dropdowns = dropdownNodes.map(function(node) {
     var dropdown = Dropdown(node);
+    dropdown.hide();
+
     return dropdown;
   });
 
-  dropdowns.forEach(function(dropdown) {
-    dropdown.control.addEventListener('click', function(event) {
-      dropdowns
-        .filter(function(dropdown) {
-          return dropdown.control !== event.target;
-        })
-        .forEach(function(d) {
-          d.hide();
-        });
-      dropdown.toggle(event);
+  function dropdownToggle(dropdown) {
+    if (!dropdown) {
+      return;
+    }
+
+    if (dropdown.isVisible) {
+      dropdown.hide();
+    } else {
+      closeAllDropdowns();
+      dropdown.show();
+    }
+  }
+
+  function closeAllDropdowns() {
+    dropdowns.forEach(function(dropdown) {
+      dropdown.hide();
     });
-  });
+  }
+
+  function hasParentNode(node, targetFn, maxDepth) {
+    var max = maxDepth || 10;
+    var depth = 1;
+    var currNode = node.parentNode;
+
+    function isMaxDepth(node, currDepth) {
+      return node.tagName === 'BODY' || currDepth === max;
+    }
+
+    while (currNode) {
+      if (isMaxDepth(currNode, depth)) {
+        currNode = null;
+        break;
+      }
+
+      if (targetFn(currNode)) {
+        break;
+      }
+
+      currNode = currNode.parentNode;
+      depth += 1;
+    }
+
+    depth = 1;
+
+    return currNode;
+  }
 
   document.body.addEventListener('click', function(event) {
-    console.log(event.target)
+    /**
+     * Determine if the click event happened inside of a dropdown.
+     * If so, there are one of two paths we need to take.
+     *
+     * If the element clicked was inside the dropdown container, do nothing.
+     *
+     * If the element clicked was exactly a dropdown button:
+     *  and the element is already open: close it
+     *  otherwise, close all dropodowns and open the one being targeted
+     *
+     * When click events occur outside of a dropdown, close all open
+     * dropdowns
+     **/
+    var node = hasParentNode(event.target, function(n) {
+      return n.classList.contains('crt-dropdown');
+    });
+
+    if (!node) {
+      closeAllDropdowns();
+    } else {
+      // A node here indicates that the click event happened within a dropdown
+      var maybeDropdown = dropdowns.find(function(dropdown) {
+        return dropdown.control === event.target;
+      });
+
+      dropdownToggle(maybeDropdown);
+    }
   });
 })(window);
