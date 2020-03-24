23b8d49cf439b3788c2303b3cd2c038ab0432bbb
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-03-24 12:37:35 -0400

    Handle filter fields which are not on model

--- crt_portal/cts_forms/templatetags/get_field_label.py
+++ crt_portal/cts_forms/templatetags/get_field_label.py
@@ -1,12 +1,19 @@
 from django import template
 from django.apps import apps
-
+from django.core.exceptions import FieldDoesNotExist
 register = template.Library()
 
 
 @register.filter(name='get_field_label')
 def get_field_label(value, arg):
-    """Return label for given model/field from cts_forms app"""
-    model = apps.get_model('cts_forms', value)
-    field = model._meta.get_field(arg)
+    """
+    Return label for given model/field from cts_forms app
+    If no field exists, we expect it's a filter field like
+    `create_date_end',  strip underscores for display
+    """
+    try:
+        model = apps.get_model('cts_forms', value)
+        field = model._meta.get_field(arg)
+    except FieldDoesNotExist:
+        return arg.replace('_', ' ')
     return field.verbose_name
