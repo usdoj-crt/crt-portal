1319c0df7e4243528b0ed1b3e96cb01e7c1e9341
lindsayyoung <lindsayn.young@gsa.gov>
2020-03-24 17:37:19 -0700

    Update migration

--- crt_portal/cts_forms/migrations/0060_add_judicial_class.py
+++ crt_portal/cts_forms/migrations/0060_add_judicial_class.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cts_forms', '0017_auto_20191118_1943'),
+        ('cts_forms', '0059_merge_20200318_2236'),
     ]
 
     operations = [
