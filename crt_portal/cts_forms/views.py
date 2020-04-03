6a3c9990846bb9a5ae442a6b07414784dc69e03c
lindsayyoung <lindsayn.young@gsa.gov>
2020-04-02 17:38:19 -0700

    Updating CRSF message and making the error icon look roughly the right size

--- crt_portal/cts_forms/views.py
+++ crt_portal/cts_forms/views.py
@@ -117,7 +117,7 @@ def csrf_failure(request, reason=""):
     return render(
         request,
         'forms/errors.html', {
-            'status': "Invalid or missing security cookie",
+            'status': "Problem with security cookie",
             'message': _("Your browser couldn't create secure cookie"),
             'helptext': _("We use security cookies to protect your information from attackers. Make sure you allow cookies for this site. Having the page open for long periods can can also cause this problem. If you know cookies are allowed and you are having this issue, try going to this page in new browser tab or window. That will make you a new security cookie and should resolve the problem.")
         }
