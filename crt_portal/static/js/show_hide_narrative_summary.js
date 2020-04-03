61930db3e1bff1890fa13fab68781c72aa2bf080
Heather <heather.battaglia@gsa.gov>
2020-04-03 16:55:50 -0600

    disable save button until typing happens

--- crt_portal/static/js/show_hide_narrative_summary.js
+++ crt_portal/static/js/show_hide_narrative_summary.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   function showSummaryForm() {
     summaryForm.classList.remove('display-none');
     summary.classList.add('display-none');
@@ -14,13 +14,27 @@
     editButton.addEventListener('click', showSummaryForm);
   }
 
+  function setButtonDisabled() {
+    if (summaryInput.value.length > 0 && saveButton.hasAttribute('disabled')) {
+      saveButton.removeAttribute('disabled');
+    } else {
+      saveButton.setAttribute('disabled', true);
+    }
+  }
+
   /* created as an empty element and populated if a summary exists */
   var summary = document.getElementById('current-summary');
 
   /* in contrast, summaryForm will always exist on the page */
   var summaryForm = document.getElementById('comment-actions-summary');
+  var summaryInput = summaryForm.getElementsByTagName('textarea')[0];
   var saveButton = summaryForm.getElementsByTagName('button')[0];
 
+  /* disable the save button on page load because there are no changes to save yet */
+  saveButton.setAttribute('disabled', 'true');
+
+  /* check to see if there's text in the summary box before making the save button active */
+  summaryInput.addEventListener('input', setButtonDisabled);
   saveButton.addEventListener('click', hideSummaryForm);
 
   if (summary.childElementCount > 0) {
