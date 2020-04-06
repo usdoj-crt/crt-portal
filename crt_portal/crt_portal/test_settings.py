223328be33a250e0d6c208b477148dda6a374570
lindsayyoung <lindsayn.young@gsa.gov>
2020-04-06 14:36:28 -0700

    It makes sense for a wildcard import in this file.

--- crt_portal/crt_portal/test_settings.py
+++ crt_portal/crt_portal/test_settings.py
@@ -4,8 +4,7 @@ from .settings import *  # noqa: F401,F403
 
 logger = logging.getLogger(__name__)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-lock_file_name = os.path.join(BASE_DIR, 'NO_ZIP.txt')
+lock_file_name = os.path.join(BASE_DIR, 'NO_ZIP.txt')  # noqa: F405
 
 with open(lock_file_name, 'w') as f:
     logger.info('Loading test settings')
