6547ef7f58f8103ea5831b42a4c836b1e0545733
lindsayyoung <lindsayn.young@gsa.gov>
2020-04-08 10:58:12 -0700

    fix dictionary look up

--- crt_portal/cts_forms/models.py
+++ crt_portal/cts_forms/models.py
@@ -132,9 +132,9 @@ class Report(models.Model):
     public_or_private_school = models.CharField(max_length=100, null=True, choices=PUBLIC_OR_PRIVATE_SCHOOL_CHOICES, default=None)
 
     # Incident date
-    last_incident_year = models.PositiveIntegerField(MaxValueValidator(datetime.now().year, message=DATE_ERRORS.no_future), null=True, blank=True)
-    last_incident_day = models.PositiveIntegerField(MaxValueValidator(31, message=DATE_ERRORS.day_invalid), null=True, blank=True)
-    last_incident_month = models.PositiveIntegerField(MaxValueValidator(12, message=DATE_ERRORS.month_invalid), null=True, blank=True,)
+    last_incident_year = models.PositiveIntegerField(MaxValueValidator(datetime.now().year, message=DATE_ERRORS['no_future']), null=True, blank=True)
+    last_incident_day = models.PositiveIntegerField(MaxValueValidator(31, message=DATE_ERRORS['day_invalid']), null=True, blank=True)
+    last_incident_month = models.PositiveIntegerField(MaxValueValidator(12, message=DATE_ERRORS['month_invalid']), null=True, blank=True,)
 
     # Internal comments
     internal_comments = models.ManyToManyField(CommentAndSummary)
