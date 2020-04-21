450e960a2a2743a52b3661d09ce728c5895fa06b
lindsayyoung <lindsayn.young@gsa.gov>
2020-04-20 20:13:57 -0700

    Simplify js

--- crt_portal/static/js/pro_form_show_hide.js
+++ crt_portal/static/js/pro_form_show_hide.js
@@ -1,16 +1,6 @@
 (function(root, dom) {
   root.CRT = root.CRT || {};
 
-  function showQuestion(question_id) {
-    var target = document.getElementById(question_id)
-    target.style.display = 'block'
-  }
-
-  function hideQuestion(question_id) {
-    var target = document.getElementById(question_id)
-    target.style.display = 'none'
-  }
-
   function toggleFollowUpQuestions(event) {
     var primary_complaint_id = event.target.id
     var predicate_target_mapping = {
@@ -19,17 +9,22 @@
      'id_0-primary_complaint_4': ['div-id_0-inside_correctional_facility_0', 'div-id_0-correctional_facility_type_0'],
      'id_0-primary_complaint_5': ['div-id_0-commercial_or_public_place_0', 'div-id_0-other_commercial_or_public_place']
     }
-
+    // show
     if(primary_complaint_id in predicate_target_mapping){
       var targets = predicate_target_mapping[primary_complaint_id]
-      targets.forEach(question_id => showQuestion(question_id))
+      targets.forEach(function(question_id){
+        var target = document.getElementById(question_id)
+        target.style.display = 'block'
+      })
     }
-
+    // hide
     Object.keys(predicate_target_mapping).forEach(function(primary_id){
-      console.log(primary_id)
       if(primary_complaint_id != primary_id){
         var targets = predicate_target_mapping[primary_id]
-        targets.forEach(question_id => hideQuestion(question_id))
+        targets.forEach(function(question_id){
+          var target = document.getElementById(question_id)
+          target.style.display = 'none'
+        })
       }
     })
   };
