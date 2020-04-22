f99754a334ef5231f128acac51ada5e1661efac0
lindsayyoung <lindsayn.young@gsa.gov>
2020-04-22 10:31:52 -0700

    Cleaned up script

--- crt_portal/static/js/details_status.js
+++ crt_portal/static/js/details_status.js
@@ -1,28 +1,22 @@
+// Announces changes when a user clicks a submit button on the CRT details page
 function announce(element) {
-    var submitButtons = {
-      'submit-summary': 'Summary submitted',
-      'submit-comment': 'Comment updated',
-      'submit-apply-changes': 'Updated activity',
-    }
-    var message = submitButtons[element.id];
-    console.log(message)
+  var submitButtons = {
+    'submit-summary': 'Summary submitted',
+    'submit-comment': 'Comment updated',
+    'submit-apply-changes': 'Updated activity'
+  };
+  var message = submitButtons[element.id];
 
-    let statusAlert = document.createElement("p");
-    statusAlert.setAttribute("role", "alert");
-    let messageText = document.createTextNode(message);
-    statusAlert.appendChild(messageText);
-    document.body.appendChild(statusAlert);
+  let statusAlert = document.createElement('p');
+  statusAlert.setAttribute('role', 'alert');
+  let alertText = document.createTextNode(message);
+  statusAlert.appendChild(alertText);
+  document.body.appendChild(statusAlert);
 }
 
 var submit_buttons = document.querySelectorAll('*[id^="submit-"]');
 submit_buttons.forEach(function(button) {
-    button.addEventListener('click', () => {
-     announce(button);
-    });
-    // I would have thought this was necessary but leads to duplicates
-    // button.addEventListener('keydown', (event) => {
-    //   if (event.code === 'Space' || event.code === 'Enter') {
-    //     announce(button);
-    //   }
-    // });
+  button.addEventListener('click', () => {
+    announce(button);
+  });
 });
