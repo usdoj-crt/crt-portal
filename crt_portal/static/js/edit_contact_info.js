9b562bc550f4ea19a2be7965a2ca77f608bef2c7
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-04-23 14:52:35 -0400

    prettier js

--- crt_portal/static/js/edit_contact_info.js
+++ crt_portal/static/js/edit_contact_info.js
@@ -1,42 +1,40 @@
 (function() {
+  function showContactForm() {
+    contactForm.classList.remove('display-none');
+    contactInfo.classList.add('display-none');
+  }
 
-    function showContactForm() {
-      contactForm.classList.remove('display-none');
-      contactInfo.classList.add('display-none');
+  function hideContactForm() {
+    contactForm.classList.add('display-none');
+    contactInfo.classList.remove('display-none');
+  }
+
+  function addShowFormHandler() {
+    var editButton = document.getElementById('edit-contact-info-btn');
+    editButton.addEventListener('click', showContactForm);
+  }
+
+  function setButtonDisabled() {
+    // Save Button only enabled if form has been modified
+    var currentState = Array.from(new FormData(contactForm), e => e[1]).join(',');
+    if (initialState === currentState) {
+      saveButton.setAttribute('disabled', true);
+    } else {
+      saveButton.removeAttribute('disabled');
     }
+  }
 
-    function hideContactForm() {
-      contactForm.classList.add('display-none');
-      contactInfo.classList.remove('display-none');
-    }
+  var contactInfo = document.getElementById('contact-info');
+  var contactForm = document.getElementById('contact-edit-form');
+  var saveButton = contactForm.getElementsByTagName('button')[0];
+  var cancelButton = contactForm.getElementsByClassName('button--cancel')[0];
 
-    function addShowFormHandler() {
-      var editButton = document.getElementById('edit-contact-info-btn');
-      editButton.addEventListener('click', showContactForm);
-    }
+  var initialState = Array.from(new FormData(contactForm), e => e[1]).join(',');
 
-    function setButtonDisabled() {
-      // Save Button only enabled if form has been modified
-      var currentState = Array.from(new FormData(contactForm),e => e[1]).join(',');
-      if (initialState === currentState) {
-        saveButton.setAttribute('disabled', true);
-      }
-      else {
-        saveButton.removeAttribute('disabled')
-      }
-    }
+  contactForm.addEventListener('input', setButtonDisabled);
 
-    var contactInfo = document.getElementById('contact-info');
-    var contactForm = document.getElementById('contact-edit-form');
-    var saveButton = contactForm.getElementsByTagName('button')[0];
-    var cancelButton = contactForm.getElementsByClassName('button--cancel')[0];
+  saveButton.addEventListener('click', hideContactForm);
+  cancelButton.addEventListener('click', hideContactForm);
 
-    var initialState = Array.from(new FormData(contactForm),e => e[1]).join(',')
-
-    contactForm.addEventListener('input', setButtonDisabled);
-
-    saveButton.addEventListener('click', hideContactForm);
-    cancelButton.addEventListener('click', hideContactForm);
-
-    addShowFormHandler();
-  })();
+  addShowFormHandler();
+})();
