d1c40639d14d8eb4dabc56dd3b41fb7b7a43adb0
Heather <heather.battaglia@gsa.gov>
2020-04-29 11:31:18 -0600

    run prettier

--- crt_portal/static/js/progress-bar.js
+++ crt_portal/static/js/progress-bar.js
@@ -1,20 +1,22 @@
 (function() {
-  var offsetHeight = document.getElementsByClassName('intake-header--progress-bar')[0].getBoundingClientRect().height;
+  var offsetHeight = document
+    .getElementsByClassName('intake-header--progress-bar')[0]
+    .getBoundingClientRect().height;
   var steps = document.getElementsByClassName('step');
   function smoothScroll(el) {
     el.preventDefault();
-    var scrollToSection = document.getElementById(el.target.attributes.href.nodeValue.slice(1, ));
+    var scrollToSection = document.getElementById(el.target.attributes.href.nodeValue.slice(1));
     var targetTop = scrollToSection.getBoundingClientRect().top;
     var totalOffset = targetTop - offsetHeight - 40; // 40px == padding on card, so title doesn't abut header
     window.scroll({
-      top: window.pageYOffset + totalOffset, 
-      left: window.pageXOffset, 
+      top: window.pageYOffset + totalOffset,
+      left: window.pageXOffset,
       behavior: 'smooth'
     });
-  };
-  for (i=0; i<steps.length; i++){
+  }
+  for (i = 0; i < steps.length; i++) {
     steps[i].addEventListener('click', function(el) {
-      smoothScroll(el)
+      smoothScroll(el);
     });
   }
-}(window));
+})(window);
