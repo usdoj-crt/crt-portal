197cb34527a35c37a7849623d045e6580a2c09e4
lindsayyoung <lindsayn.young@gsa.gov>
2020-05-21 08:30:10 -0700

    Remove unused import

--- crt_portal/crt_portal/urls.py
+++ crt_portal/crt_portal/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView, TemplateView
+from django.views.generic import RedirectView
 
 from cts_forms.forms import (
     Contact,
