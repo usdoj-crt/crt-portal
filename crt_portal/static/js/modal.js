a7e287490fae445cfb105c2c9e229f99921cffeb
James Tranovich <james.tranovich@gsa.gov>
2020-06-02 15:36:36 -0700

    template: run prettier

--- crt_portal/static/js/modal.js
+++ crt_portal/static/js/modal.js
@@ -1,65 +1,65 @@
 (function() {
   var previous_onkeydown = document.onkeydown;
 
-  var modal = document.getElementById("intake_template");
+  var modal = document.getElementById('intake_template');
 
   function openModal() {
     document.onkeydown = function(event) {
       event = event || window.event;
       var isEscape = false;
-      if ("key" in event) {
-        isEscape = (event.key === "Escape" || event.key === "Esc");
+      if ('key' in event) {
+        isEscape = event.key === 'Escape' || event.key === 'Esc';
       } else {
-        isEscape = (event.keyCode === 27);
+        isEscape = event.keyCode === 27;
       }
       if (isEscape) {
         closeModal();
       }
     };
-    modal.removeAttribute("hidden");
-    document.body.classList.add("is-modal");
+    modal.removeAttribute('hidden');
+    document.body.classList.add('is-modal');
   }
 
   function closeModal() {
     document.onkeydown = previous_onkeydown;
-    modal.setAttribute("hidden", "hidden");
-    document.body.classList.remove("is-modal");
+    modal.setAttribute('hidden', 'hidden');
+    document.body.classList.remove('is-modal');
   }
 
-  var contact = document.getElementById("contact_complainant");
+  var contact = document.getElementById('contact_complainant');
   contact.onclick = function(event) {
     event.preventDefault();
-    if (modal.getAttribute("hidden") !== null) {
+    if (modal.getAttribute('hidden') !== null) {
       openModal();
     } else {
       closeModal();
     }
   };
 
-  var cancel_modal = document.getElementById("intake_template_cancel");
+  var cancel_modal = document.getElementById('intake_template_cancel');
   cancel_modal.onclick = function(event) {
     event.preventDefault();
     closeModal();
   };
 
-  var copy = document.getElementById("intake_copy");
+  var copy = document.getElementById('intake_copy');
   copy.onclick = function(event) {
     event.preventDefault();
   };
 
-  var options = document.getElementById("intake_select");
-  var letter = document.getElementById("intake_letter");
-  var description = document.getElementById("intake_description");
+  var options = document.getElementById('intake_select');
+  var letter = document.getElementById('intake_letter');
+  var description = document.getElementById('intake_description');
   options.onchange = function(event) {
     event.preventDefault();
     var index = event.target.selectedIndex;
     var option = event.target.options[index];
-    description.innerHTML = option.dataset["description"];
-    letter.innerHTML = option.dataset["content"];
+    description.innerHTML = option.dataset['description'];
+    letter.innerHTML = option.dataset['content'];
     if (index >= 1) {
-      copy.removeAttribute("disabled");
+      copy.removeAttribute('disabled');
     } else {
-      copy.setAttribute("disabled", "disabled");
+      copy.setAttribute('disabled', 'disabled');
     }
   };
 })();
