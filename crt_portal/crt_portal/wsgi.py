3e7051119ce093db7b8fda91fcd3a12caa17dad1
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-06-17 15:36:23 -0400

    Initialize new relic first

--- crt_portal/crt_portal/wsgi.py
+++ crt_portal/crt_portal/wsgi.py
@@ -7,14 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+import newrelic.agent
 import os
 import json
 import logging
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crt_portal.settings')
-
 logger = logging.getLogger(__name__)
 
 
@@ -36,7 +32,6 @@ def initialize_newrelic():
     license_key = get_newrelic_key()
 
     if license_key:
-        import newrelic.agent
         logger.info('New Relic key found, initializing...')
         settings = newrelic.agent.global_settings()
         settings.license_key = license_key
@@ -45,5 +40,9 @@ def initialize_newrelic():
 
 
 initialize_newrelic()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crt_portal.settings')
+
+from django.core.wsgi import get_wsgi_application # noqa
 
 application = get_wsgi_application()
