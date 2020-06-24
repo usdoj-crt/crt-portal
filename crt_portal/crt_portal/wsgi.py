5b5fe8c5cba544e5703b6b6451294e1b47b89ef7
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-06-23 21:21:20 -0400

    Newrelic init logging and key handling
    We should fault because of a missing newrelic key.
    If we expect a key, and none is
    found write a warning message to the logs.

--- crt_portal/crt_portal/wsgi.py
+++ crt_portal/crt_portal/wsgi.py
@@ -13,6 +13,8 @@ import json
 import logging
 logger = logging.getLogger(__name__)
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crt_portal.settings')
+
 
 def get_newrelic_key():
     """
@@ -21,27 +23,28 @@ def get_newrelic_key():
     """
     environment = os.environ.get('ENV', 'UNDEFINED')
     if environment in ['STAGE', 'DEVELOP', 'PRODUCTION']:
+        logger.warning('Looking for New Relic key...')
         vcap = json.loads(os.environ['VCAP_SERVICES'])
         for service in vcap['user-provided']:
             if service['instance_name'] == "VCAP_SERVICES":
-                return service['credentials']['NEW_RELIC_LICENSE_KEY']
+                key = service['credentials'].get('NEW_RELIC_LICENSE_KEY')
+                if not key:
+                    logger.warning('No New Relic Key found')
+                return key
 
 
 def initialize_newrelic():
     """Initialize NewRelic instrumentation if we have a key"""
     license_key = get_newrelic_key()
-
     if license_key:
-        logger.info('New Relic key found, initializing...')
+        logger.warning('New Relic key found, initializing...')
         settings = newrelic.agent.global_settings()
         settings.license_key = license_key
         newrelic.agent.initialize()
-        logger.info('New Relic initialized')
+        logger.warning('New Relic initialized')
 
 
 initialize_newrelic()
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crt_portal.settings')
 
 from django.core.wsgi import get_wsgi_application # noqa
 
