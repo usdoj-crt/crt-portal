ef28660bfb70b45adef6695cd22dbca485601bc8
James Tranovich <james.tranovich@gsa.gov>
2020-08-06 11:50:50 -0700

    landing redirect: accommodate multiple external links

--- crt_portal/static/js/redirect-modal.js
+++ crt_portal/static/js/redirect-modal.js
@@ -2,23 +2,25 @@
   // note that modal.js must be loaded beforehand
   var modal_el = document.getElementById('external-link--modal');
   var span = document.getElementById('external-link--address');
-  var link = document.querySelector('.external-link--popup');
+  var links = document.querySelectorAll('.external-link--popup');
   var redirect;
-  link.onclick = function(event) {
-    event.preventDefault();
-    // display the actual redirect link
-    span.href = link.href;
-    span.innerText = link.href;
-    root.CRT.openModal(modal_el);
-    // set timeout for redirect
-    clearTimeout(redirect);
-    redirect = setTimeout(function() {
-      // only redirect if modal is still visible
-      if (modal_el.getAttribute('hidden') === null) {
-        window.location.href = link.href;
-      }
-    }, 20000);
-  };
+  links.forEach(function(link) {
+    link.onclick = function(event) {
+      event.preventDefault();
+      // display the actual redirect link
+      span.href = link.href;
+      span.innerText = link.href;
+      root.CRT.openModal(modal_el);
+      // set timeout for redirect
+      clearTimeout(redirect);
+      redirect = setTimeout(function() {
+        // only redirect if modal is still visible
+        if (modal_el.getAttribute('hidden') === null) {
+          window.location.href = link.href;
+        }
+      }, 20000);
+    };
+  });
   var cancel_modal = document.getElementById('external-link--cancel');
   root.CRT.cancelModal(modal_el, cancel_modal);
   var continue_button = document.getElementById('external-link--continue');
