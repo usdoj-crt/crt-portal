4c70e000286fe31f4e6436fb8832619cad1e490e
James Tranovich <james.tranovich@gsa.gov>
2020-08-07 08:31:37 -0700

    landing redirect: fix per link href

--- crt_portal/static/js/redirect-modal.js
+++ crt_portal/static/js/redirect-modal.js
@@ -8,23 +8,25 @@
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
     link.onclick = function(event) {
+      var href = event.target.href;
       event.preventDefault();
       // display the actual redirect link
-      span.innerHTML = '<a href="' + link.href + '">' + link.href + '</a>';
+      span.innerHTML = '<a href="' + href + '">' + href + '</a>';
       root.CRT.openModal(modal_el);
       // set timeout for redirect
       clearTimeout(redirect);
       redirect = setTimeout(function() {
         // only redirect if modal is still visible
         if (modal_el.getAttribute('hidden') === null) {
-          window.location.href = link.href;
+          window.location.href = href;
         }
       }, 20000);
 
       // set up "continue" button to immediately redirect
       continue_button.onclick = function(event) {
         event.preventDefault();
-        window.location.href = link.href;
+        var href = span.children[0].href;
+        window.location.href = href;
       };
     };
   }
