c281a6588b07dec91d17dee6ec2ec7c5814d531b
James Tranovich <james.tranovich@gsa.gov>
2020-09-03 15:11:23 -0700

    quickview: caret toggle

--- crt_portal/static/js/complaint_quick_view.js
+++ crt_portal/static/js/complaint_quick_view.js
@@ -1,3 +1,15 @@
 (function(root, dom) {
-  console.log('foo');
+  var toggles = dom.querySelectorAll('.td-toggle');
+  for (var i = 0; i < toggles.length; i++) {
+    var toggle = toggles[i];
+    toggle.onclick = function(event) {
+      var target = event.target;
+      if (target.classList.contains('rotate')) {
+        target.classList.remove('rotate');
+      } else {
+        target.classList.add('rotate');
+      }
+      event.preventDefault();
+    };
+  }
 })(window, document);
