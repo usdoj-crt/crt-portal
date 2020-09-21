d4906123231a97f85f326c5118cbb098b0c26d84
James Tranovich <james.tranovich@gsa.gov>
2020-09-21 11:51:01 -0700

    Bugfix: updating assigned_to shows id of original user

--- crt_portal/cts_forms/forms.py
+++ crt_portal/cts_forms/forms.py
@@ -1096,7 +1096,12 @@ class ComplaintActions(ModelForm, ActivityStreamUpdater):
             # rename primary statute if applicable
             if field == 'primary_statute':
                 name = 'Primary classification'
-            yield f"{name}:", f'Updated from "{self.initial[field]}" to "{self.cleaned_data[field]}"'
+            original = self.initial[field]
+            changed = self.cleaned_data[field]
+            # fix bug where id was showing up instead of user name
+            if field == 'assigned_to':
+                original = User.objects.get(id=original)
+            yield f"{name}:", f'Updated from "{original}" to "{changed}"'
         if self.report_closed:
             yield "Report closed and Assignee removed", f"Date closed updated to {self.instance.closed_date.strftime('%m/%d/%y %H:%M:%M %p')}"
 
