4edf7d091a84d25599f06163ad58047325388d51
hakhalid11 <haseeb.khalid@usdoj.gov>
2020-10-16 11:09:35 -0400

    404 page fully spanish translated

--- crt_portal/cts_forms/views.py
+++ crt_portal/cts_forms/views.py
@@ -71,7 +71,7 @@ def error_404(request, exception=None):
     return render(
         request,
         'forms/errors.html', {
-            'status': '404 | Page not found ',
+            'status': _("404 | Page not found"),
             'message': _("We can't find the page you are looking for")
         },
         status=404
