b52e2834c9b939bc292a52512c13ec464c299d89
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-11-02 10:11:24 -0500

    Add internal summary to Report export
    We now include the latest internal summary in each
    export Report row.
    
    Tackling this with prefetch_related, limited to summaries, since
    these exist as a M2M.

--- crt_portal/cts_forms/admin.py
+++ crt_portal/cts_forms/admin.py
@@ -4,6 +4,7 @@ import logging
 from django.contrib import admin
 from django.core.paginator import Paginator
 from django.http import StreamingHttpResponse
+from django.db.models import Prefetch
 
 from .models import (CommentAndSummary, HateCrimesandTrafficking, Profile,
                      ProtectedClass, Report, ResponseTemplate)
@@ -50,33 +51,40 @@ def iter_queryset(queryset, headers):
 
 def _serialize_report_export(data):
     """
-    Customize the rendering of protected_class instances
+    Customize the rendering of protected_class and summary instances
     while rendering headers as-is
     """
     if isinstance(data, Report):
         row = [getattr(data, field) for field in REPORT_FIELDS]
         row.append('; '.join([str(pc) for pc in data.protected_class.all()]))
+        if data.internal_summary:
+            # incoming summaries are sorted by descending modified_date the first is the most recent
+            row.append(data.internal_summary[0].note)
         return row
     return data
 
 
 def export_as_csv(modeladmin, request, queryset):
     """
-    Stream all non-related fields
-    and the protected_class M2M of selected reports as a CSV
+    Stream all non-related fields,
+    protected_class M2M,
+    and latest summary from CommentAndSummary M2M of selected reports as a CSV
     Log all use
     """
     writer = csv.writer(Echo(), quoting=csv.QUOTE_ALL)
-    headers = REPORT_FIELDS + ['protected_class']
+    headers = REPORT_FIELDS + ['protected_class', 'internal_summary']
 
-    queryset = queryset.prefetch_related('protected_class').order_by('id')
+    summaries = CommentAndSummary.objects.filter(is_summary=True).order_by('-modified_date')
+    queryset = queryset.prefetch_related('protected_class',
+                                         Prefetch('internal_comments', queryset=summaries, to_attr='internal_summary')
+                                         ).order_by('id')
     iterator = iter_queryset(queryset, headers)
 
     response = StreamingHttpResponse((writer.writerow(_serialize_report_export(report)) for report in iterator),
                                      content_type="text/csv")
     response['Content-Disposition'] = 'attachment; filename="report_export.csv"'
 
-    logger.info(format_export_message(request, queryset.count() - 1))
+    logger.info(format_export_message(request, queryset.count()))
     return response
 export_as_csv.allowed_permissions = ('view',)  # noqa
 
