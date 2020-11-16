6815312d720d193ab83b875e0861bf3b25030fe4
Tim Ballard <timothy.ballard@gsa.gov>
2020-11-16 11:10:18 -0600

    summarize selected section filters

---
+++ crt_portal/cts_forms/tests/test_templatetags.py
@@ -0,0 +1,21 @@
+from django.template import Template, Context
+from django.test import TestCase
+
+class MultiselectSummaryTest(TestCase):
+  TEMPLATE = Template('{% load multiselect_summary %} {{ selected|multiselect_summary:"default text" }} ')
+  
+  def test_none_selected(self):
+    rendered = self.TEMPLATE.render(Context({ "selected": "" }))
+    self.assertIn("default text", rendered)
+
+  def test_one_selected(self):
+    rendered = self.TEMPLATE.render(Context({ "selected": "One" }))
+    self.assertIn("One", rendered)
+
+  def test_two_selected(self):
+    rendered = self.TEMPLATE.render(Context({ "selected": "One,Two" }))
+    self.assertIn("One, Two", rendered)
+
+  def test_three_selected(self):
+    rendered = self.TEMPLATE.render(Context({ "selected": "One,Two,Three" }))
+    self.assertIn("Multi (3)", rendered)
