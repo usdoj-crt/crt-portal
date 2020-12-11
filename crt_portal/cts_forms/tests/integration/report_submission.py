b9b3da727b0fdd1d3a91b28ce08e61b93b1639eb
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-12-11 13:05:32 -0500

    Add playwright test for 422 error

--- crt_portal/cts_forms/tests/integration/report_submission.py
+++ crt_portal/cts_forms/tests/integration/report_submission.py
@@ -3,6 +3,33 @@ import pytest
 
 
 @pytest.mark.only_browser("chromium")
+def test_error_if_form_refreshed(page, base_url):
+
+    def next_step():
+        with page.expect_navigation() as response:
+            page.click('input[type="submit"]')
+        return response.value
+
+    page.goto("/report")
+    # We'll complete the first step
+    page.check("input[name='0-servicemember']")
+    next_step()
+
+    # Refresh the page in another tab
+    new_tab = page.context.newPage()
+    new_tab.goto(f"{base_url}/report")
+
+    # Now try and progress on original tab where we're still on step #2
+    assert page.title() == "Step 2: Primary concern - Contact the Civil Rights Division | Department of Justice"  # nosec
+    page.check("#id_1-primary_complaint_0")
+    response = next_step()
+
+    # Now we've got an error page on the original tab
+    assert response.status == 422
+    assert "We're sorry, something went wrong." in response.text()
+
+
+@pytest.mark.only_browser("chromium")
 def test_report_complete_and_valid_submission(page):
 
     def next_step():
