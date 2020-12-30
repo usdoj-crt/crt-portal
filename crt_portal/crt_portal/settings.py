d9ac96d2283d9686e1265c0cb30e70046ac76975
James Tranovich <james.tranovich@gsa.gov>
2020-12-30 12:57:51 -0800

    compressor redux: use django-storage alternative instead

--- crt_portal/crt_portal/settings.py
+++ crt_portal/crt_portal/settings.py
@@ -248,7 +248,7 @@ if environment not in ['LOCAL', 'UNDEFINED']:
     AWS_LOCATION = 'static'
     AWS_QUERYSTRING_AUTH = False
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'crt_portal.storage.ManifestS3FilesStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_DEFAULT_ACL = 'public-read'
     AWS_IS_GZIPPED = True
