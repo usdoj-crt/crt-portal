483a788db2dc33563b03c209255dfad3df28b16e
Tim Ballard <timothy.ballard@gsa.gov>
2021-02-15 16:54:25 -0600

    flake8 fixes

--- crt_portal/cts_forms/validators.py
+++ crt_portal/cts_forms/validators.py
@@ -8,20 +8,21 @@ logger = logging.getLogger(__name__)
 
 
 def _scan_file(file):
-  return requests.post(settings.AV_SCAN_URL, files={'file': file}, data={'name': file.name})
+    return requests.post(settings.AV_SCAN_URL, files={'file': file}, data={'name': file.name})
+
 
 def validate_file_infection(file):
     logger.info(f'Attempting to scan file: {file}.')
 
     attempt = 1
     while (res := _scan_file(file)).status_code == 500:
-      if (attempt := attempt + 1) > settings.AV_SCAN_MAX_ATTEMPTS:
-        break
+        if (attempt := attempt + 1) > settings.AV_SCAN_MAX_ATTEMPTS:
+            break
 
-      logger.info(f'Scan attempt {attempt} failed, trying again...')
-      file.seek(0)
-    
-    if not settings.AV_SCAN_SUCCESS_STR in res.text:
-      raise ValidationError('The file you uploaded did not pass our security inspection, attachment failed!')
+        logger.info(f'Scan attempt {attempt} failed, trying again...')
+        file.seek(0)
+
+    if settings.AV_SCAN_SUCCESS_STR not in res.text:
+        raise ValidationError('The file you uploaded did not pass our security inspection, attachment failed!')
 
     logger.info(f'Scanning of file {file} complete.')
