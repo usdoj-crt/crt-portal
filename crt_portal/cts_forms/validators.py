09110fb7b6370aaf96d77d0ddeec9cb4d4f07165
Tim Ballard <timothy.ballard@gsa.gov>
2021-02-22 10:16:32 -0600

    refactor validators.py

--- crt_portal/cts_forms/validators.py
+++ crt_portal/cts_forms/validators.py
@@ -15,6 +15,44 @@ AV_SCAN_CODES = {
     'ERROR': [400, 412, 500, 501],
 }
 
+MAX_FILE_SIZE_MB = 100
+ALLOWED_CONTENT_TYPES = [
+    'application/msword',
+    'application/pdf',
+    'application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'audio/mpeg',
+    'audio/wav',
+    'audio/x-aiff',
+    'image/bmp',
+    'image/jpeg',
+    'image/gif',
+    'image/png',
+    'image/tiff',
+    'text/csv',
+    'text/plain',
+]
+
+ALLOWED_FILE_EXTENTIONS = [
+    'aiff',
+    'bmp',
+    'csv',
+    'doc',
+    'docx',
+    'gif',
+    'pdf',
+    'jpeg',
+    'jpg',
+    'mp3',
+    'png',
+    'tif',
+    'txt',
+    'wav',
+    'xls',
+    'xlsx',
+]
+
 
 def _scan_file(file):
     return requests.post(settings.AV_SCAN_URL, files={'file': file}, data={'name': file.name})
@@ -43,34 +81,24 @@ def validate_file_infection(file):
 
 
 def validate_file_size(file):
-    # Maximum file size allowed: 100 MB
     file_size = round((file.size / 1024 / 1024), 2)
-    max_mb = 100
 
-    if file_size > max_mb:
-        raise ValidationError(f'This file size is: {file_size} MB this cannot be uploaded, maximum allowed: {max_mb} MB ')
+    if file_size > MAX_FILE_SIZE_MB:
+        raise ValidationError(f'This file size is: {file_size} MB this cannot be uploaded, maximum allowed: {MAX_FILE_SIZE_MB} MB ')
 
 
 def validate_content_type(file):
-    # Supported content types: image/bmp, text/csv, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document,iimage/jpeg, image/gif, audio/mpeg, image/png, application/pdf, mage/tiff, text/plain, audio/wav, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
-
-    valid_content_types = ('image/bmp', 'text/csv', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'image/jpeg', 'image/gif', 'audio/mpeg', 'image/png', 'application/pdf', 'image/tiff', 'text/plain', 'audio/wav', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'audio/x-aiff')
-
     file_content_type = file.file.content_type
 
-    if file_content_type not in valid_content_types:
-        raise ValidationError(f'File content type: {file_content_type} not supported for upload, supported content types are: {valid_content_types}')
+    if file_content_type not in ALLOWED_CONTENT_TYPES:
+        raise ValidationError(f'File content type: {file_content_type} not supported for upload, supported content types are: {ALLOWED_CONTENT_TYPES}')
 
 
 def validate_file_extension(file):
-
-    # valid file extensions PDF, JPG, GIF, BMP, TIF, PNG, AIFF, WAV, MP3, DOC, DOCX, XLS, XLSX, CSV, TXT
-
-    valid_file_extension = ('pdf', 'jpg', 'gif', 'bmp', 'tif', 'png', 'aiff', 'wav', 'mp3', 'doc', 'docx', 'xls', 'xlsx', 'csv', 'txt')
     this_file_extension = os.path.splitext(file.name)[1][1:].lower()
 
-    if this_file_extension not in valid_file_extension:
-        raise ValidationError(f'File extension: {this_file_extension} not supported for upload, supported extensions are: {valid_file_extension}')
+    if this_file_extension not in ALLOWED_FILE_EXTENTIONS:
+        raise ValidationError(f'File extension: {this_file_extension} not supported for upload, supported extensions are: {ALLOWED_FILE_EXTENTIONS}')
 
 
 def validate_file_attachment(file):
