700729b58677273d3334030c0dbb1d10bec31a82
Tim Ballard <timothy.ballard@gsa.gov>
2021-03-10 10:38:53 -0600

    add confirmation modal for attachment removals

--- crt_portal/static/js/attachments.js
+++ crt_portal/static/js/attachments.js
@@ -14,4 +14,44 @@
     var modal = document.getElementById('attachment-uploading--modal');
     root.CRT.openModal(modal);
   };
+
+  var remove_buttons = document.querySelectorAll('.remove-attachment-button');
+  remove_buttons.forEach(function(btn) {
+    btn.onclick = function(event) {
+      event.preventDefault();
+
+      var attachment_id = this.getAttribute('data-attachment-id');
+      var attachment_filename = this.getAttribute('data-attachment-filename');
+
+      // modal elements
+      var modal = document.getElementById('attachment-removal-confirmation--modal');
+      var filename_el = document.getElementById('attachment-removal--filename');
+      var no_button = document.getElementById('attachment-removal--no');
+      var yes_button = document.getElementById('attachment-removal--yes');
+      
+      // add the to-be-removed filename to the modal
+      filename_el.innerText = attachment_filename;
+
+      // user clicks no on removal confirmation
+      no_button.onclick = function(event) {
+        event.preventDefault();
+        // do nothing, just hide the modal
+        root.CRT.closeModal(modal);
+      };
+
+      // user clicks yes on removal confirmation
+      yes_button.onclick = function(event) {
+        event.preventDefault();
+        
+        // get the form for removing this particular attachment
+        var form_id = `complaint-view-remove-attachment-${attachment_id}`;
+        var form = document.getElementById(form_id);
+
+        // submit the removal form
+        form.submit();
+      };
+
+      root.CRT.openModal(modal);
+    };
+  });
 })(window, document);
