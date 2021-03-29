263b21fbac05a1ff125f083393a93d3d9246d943
Tim Ballard <timothy.ballard@gsa.gov>
2021-03-29 12:40:08 -0500

    add language selection links to public-facing banner

---
+++ crt_portal/static/js/banner_language_selection.js
@@ -0,0 +1,20 @@
+(function(root, dom) {
+  var buttons = document.querySelectorAll('.language-selection__button');
+  buttons.forEach(function(button) {
+    button.onclick = function(event) {
+      console.log('handling button click');
+      event.preventDefault();
+
+      var language_code = this.getAttribute('data-value');
+      console.log(language_code);
+
+      var language_input_el = document.getElementById('language_input');
+
+      language_input_el.setAttribute('value', language_code);
+
+      var form = document.getElementById('language_selection_form');
+      form.submit();
+    };
+  });
+
+})(window, document);
