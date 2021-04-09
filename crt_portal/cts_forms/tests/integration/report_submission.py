655a7ea751dc16d104c2e0253841bf6fac85b565
imran lodi <imran.lodi@usdoj.gov>
2021-04-09 14:12:43 -0400

    added play wright test for footer check

--- crt_portal/cts_forms/tests/integration/report_submission.py
+++ crt_portal/cts_forms/tests/integration/report_submission.py
@@ -77,6 +77,16 @@ def test_report_complete_and_valid_submission(page):
     next_step()
     assert page.title() == "Step 2: Primary concern - Contact the Civil Rights Division | Department of Justice"
 
+    # Check footer exist
+    assert page.footer() == "Links"
+
+    # Check footer exist
+    content = page.text_content("footer")
+    assert content == "Links"
+
+    # Check privacy footer
+    assert page.footer("privacy-footer") == "Privacy Policy"
+
     # Check NOT hatecrime
     page.check("#id_2-hate_crime_1")
 
