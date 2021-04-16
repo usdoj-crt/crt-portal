65d32737a8983d035e98cb5384ec9fef1339bfcc
Joe Krzystan <joseph.krzystan@gsa.gov>
2021-04-16 09:56:05 -0400

    Restrict email recipients when not in production
    All TMS api endpoints send emails, we'll configure allowable
    domains to help prevent accidental emails from
    development or staging instances.

--- crt_portal/tms/backend/__init__.py
+++ crt_portal/tms/backend/__init__.py
@@ -10,11 +10,14 @@ class TMSEmailBackend(BaseEmailBackend):
     """
     A wrapper that manages the calls and responses to the TMS API
     """
-    EMAIL_ENDPOINT = '/messages/email'
 
-    def __init__(self, api_root=None, api_auth_token=None, fail_silently=False, **kwargs):
+    EMAIL_ENDPOINT = "/messages/email"
+
+    def __init__(
+        self, api_root=None, api_auth_token=None, fail_silently=False, **kwargs
+    ):
         super().__init__(fail_silently=fail_silently)
-        self.api_root = api_root or settings.TMS_ENDPOINT
+        self.api_root = api_root or settings.TMS_TARGET_ENDPOINT
         self.api_auth_token = api_auth_token or settings.TMS_AUTH_TOKEN
         self.connection = None
 
@@ -25,7 +28,9 @@ class TMSEmailBackend(BaseEmailBackend):
         if self.connection:
             return False
         try:
-            self.connection = TMSClient(api_root=self.api_root, api_auth_token=self.api_auth_token)
+            self.connection = TMSClient(
+                api_root=self.api_root, api_auth_token=self.api_auth_token
+            )
         except Exception:
             if not self.fail_silently:
                 raise
@@ -52,27 +57,48 @@ class TMSEmailBackend(BaseEmailBackend):
         """
         Establish outbound payload for TMS send email POST API
         """
-        recipients = [{'email': to_address} for to_address in email_message.to]
-        return {'subject': email_message.subject,
-                'body': email_message.body,
-                'recipients': recipients,
-                'open_tracking_enabled': False,
-                'click_tracking_enabled': False
-                }
+        recipients = [{"email": to_address} for to_address in email_message.to]
+        return {
+            "subject": email_message.subject,
+            "body": email_message.body,
+            "recipients": recipients,
+            "open_tracking_enabled": False,
+            "click_tracking_enabled": False,
+        }
+
+    def _remove_disallowed_recipients(self, email_message):
+        """
+        If we've limited the recipients, modify the `to` attribute of our email_message
+        """
+        # If the list of restrictions is empty, allow all
+        restricted_to = settings.RESTRICT_EMAIL_RECIPIENT_DOMAINS_TO
+        if restricted_to:
+            email_message.to = [
+                to_address
+                for to_address in email_message.to
+                if to_address.split("@")[1] in restricted_to
+            ]
+        return email_message
 
     def _send(self, email_message):
         """
         A helper method that does the actual sending.
         """
-        if not email_message.recipients():
+        outbound_message = self._remove_disallowed_recipients(email_message)
+        if not outbound_message.recipients():
             return {}
-        outbound = self._prepare_outbound_dict(email_message)
-        response = self.connection.post(target=self.EMAIL_ENDPOINT, payload=outbound)
+        outbound_data = self._prepare_outbound_dict(outbound_message)
+        response = self.connection.post(target=self.EMAIL_ENDPOINT, payload=outbound_data)
+
         if response.status_code == 201:
             # Success per TMS docs
-            logger.debug(f'TMS API email success:<{response.status_code}> {response.json()}')
+            logger.debug(
+                f"TMS API email success:<{response.status_code}> {response.json()}"
+            )
         else:
-            logger.warn(f'TMS API email send failed:<{response.status_code}> {response.json()}')
+            logger.warn(
+                f"TMS API email send failed:<{response.status_code}> {response.json()}"
+            )
             if not self.fail_silently:
                 raise
         return response.json()
--- crt_portal/tms/backend/tests.py
+++ crt_portal/tms/backend/tests.py
@@ -1,5 +1,5 @@
-from django.core import mail
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, override_settings
+from django.core.mail import EmailMessage
 
 from ..api.client import TMSClient
 from . import TMSEmailBackend
@@ -14,3 +14,17 @@ class TMSEmailBackendTests(SimpleTestCase):
         """Connection used for sending emails is a TMSClient instance"""
         self.backend.open()
         self.assertTrue(isinstance(self.backend.connection, TMSClient))
+
+    @override_settings(RESTRICT_EMAIL_RECIPIENT_DOMAINS_TO=['test.com'])
+    def test__remove_disallowed_recipients(self):
+        """
+        Targets for email messages are removed if the domain is not found in
+        the recipients list
+        """
+        email = EmailMessage('Subject',
+                             'Body',
+                             'from@example.com',
+                             ['to1@example.com', 'to2@test.com'],
+                             )
+        processed_email = self.backend._remove_disallowed_recipients(email)
+        self.assertEquals(processed_email.to, ['to2@test.com'])
