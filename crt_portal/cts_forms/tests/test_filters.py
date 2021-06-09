412934c403324caa28da08064de3318e5886d69b
imran lodi <imran.lodi@usdoj.gov>
2021-06-09 16:28:25 -0400

    added tests for language search filter

--- crt_portal/cts_forms/tests/test_filters.py
+++ crt_portal/cts_forms/tests/test_filters.py
@@ -28,6 +28,50 @@ class ReportFilterTests(TestCase):
         r2.protected_class.add(age)
         r2.protected_class.add(gender)
 
+        # http://0.0.0.0:8000/form/view/?status=new&status=open&no_status=false&language=es&language=en&language=zh-hant&language=zh-hans&language=vi&language=ko&language=tl
+
+        # test setup for language English
+        r3 = Report.objects.create(**test_data)
+        r3.protected_class.add(age)
+        r3.protected_class.add(gender)
+        test_data['language'] = 'en'
+
+        # test setup for language Spanish
+        r4 = Report.objects.create(**test_data)
+        r4.protected_class.add(age)
+        r4.protected_class.add(gender)
+        test_data['language'] = 'es'
+
+        # test setup for language Chinese traditional
+        r5 = Report.objects.create(**test_data)
+        r5.protected_class.add(age)
+        r5.protected_class.add(gender)
+        test_data['language'] = 'zh-hant'
+
+        # test setup for language Chinese simplified
+        r6 = Report.objects.create(**test_data)
+        r6.protected_class.add(age)
+        r6.protected_class.add(gender)
+        test_data['language'] = 'zh-hans'
+
+        # test setup for language Vietnamese
+        r7 = Report.objects.create(**test_data)
+        r7.protected_class.add(age)
+        r7.protected_class.add(gender)
+        test_data['language'] = 'vi'
+
+        # test setup for language Korean
+        r8 = Report.objects.create(**test_data)
+        r8.protected_class.add(age)
+        r8.protected_class.add(gender)
+        test_data['language'] = 'ko'
+
+        # test setup for language tagalog
+        r9 = Report.objects.create(**test_data)
+        r9.protected_class.add(age)
+        r9.protected_class.add(gender)
+        test_data['language'] = 'tl'
+
     def test_no_filters(self):
         """Returns all reports when no filters provided"""
         reports, _ = report_filter(QueryDict(''))
@@ -41,11 +85,41 @@ class ReportFilterTests(TestCase):
         self.assertEquals(reports.count(), 1)
 
         reports, _ = report_filter(QueryDict('violation_summary=plane&violation_summary=truck'))
-        self.assertEquals(reports.count(), 2)
+        self.assertEquals(reports.count(), 9)
 
     def test_reported_reason(self):
         reports, _ = report_filter(QueryDict('reported_reason=age'))
-        self.assertEquals(reports.count(), 2)
+        self.assertEquals(reports.count(), 9)
 
         reports, _ = report_filter(QueryDict('reported_reason=gender&reported_reason=language'))
+        self.assertEquals(reports.count(), 8)
+
+    # language=es&language=en&language=zh-hant&language=zh-hans&language=vi&language=ko&language=tl
+    # report language filter test
+    def test_reported_language_en(self):
+        reports, _ = report_filter(QueryDict('language=en'))
+        self.assertEquals(reports.count(), 4)
+
+    def test_reported_language_es(self):
+        reports, _ = report_filter(QueryDict('language=es'))
+        self.assertEquals(reports.count(), 1)
+
+    def test_reported_language_hant(self):
+        reports, _ = report_filter(QueryDict('language=zh-hant'))
+        self.assertEquals(reports.count(), 1)
+
+    def test_reported_language_hans(self):
+        reports, _ = report_filter(QueryDict('language=zh-hans'))
+        self.assertEquals(reports.count(), 1)
+
+    def test_reported_language_vi(self):
+        reports, _ = report_filter(QueryDict('language=vi'))
+        self.assertEquals(reports.count(), 1)
+
+    def test_reported_language_ko(self):
+        reports, _ = report_filter(QueryDict('language=ko'))
+        self.assertEquals(reports.count(), 1)
+
+    def test_reported_language_tl(self):
+        reports, _ = report_filter(QueryDict('language=tl'))
         self.assertEquals(reports.count(), 1)
