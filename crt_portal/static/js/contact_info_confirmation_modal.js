427ecb0baea1979891a19cc50d22445a3d1c600f
Billy Hunt <billyhunt@navapbc.com>
2021-07-19 10:31:56 -0400

    Add cleaned up modal and start to add button events

--- crt_portal/static/js/contact_info_confirmation_modal.js
+++ crt_portal/static/js/contact_info_confirmation_modal.js
@@ -2,16 +2,14 @@
   // note that modal.js must be loaded beforehand
   var modal_el = document.getElementById('contact-info-confirmation--modal');
   var span = document.getElementById('external-link--address');
-  var links = document.getElementById('submit-next');
-  console.log('links', links)
+  var links = document.querySelectorAll('.external-link--popup');
   var continue_button = document.getElementById('external-link--continue');
   var redirect;
-
-  console.log('in contact-info-confirmaton-model')
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
     console.log('links', links);
     link.onclick = function(event) {
+      console.log('document.getElementsByName(\'0-contact_email\')[0].value', document.getElementsByName('0-contact_email')[0].value)
       console.log('in link.onclick')
       var href = event.target.href;
       event.preventDefault();
@@ -39,9 +37,12 @@
   root.CRT.cancelModal(modal_el, cancel_modal);
 
   if (root.CRT.stageNumber === 1) {
-
+    submitButton = document.getElementById('submit-next')
+    submitButton.onclick((event) => {
+      console.log(event)
+      // root.CRT.openModal(contactInfoConfirm);
+    })
     var contactInfoConfirm = document.getElementById('contact-info-confirmation--modal');
-    root.CRT.openModal(contactInfoConfirm);
   }
 
 })(window);
