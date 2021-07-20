76e6fbed7404f081e7a7d8d6af569d112ebb011a
Billy Hunt <billyhunt@navapbc.com>
2021-07-20 16:57:28 -0400

    Update with prettier

--- crt_portal/static/js/contact_info_confirmation_modal.js
+++ crt_portal/static/js/contact_info_confirmation_modal.js
@@ -3,46 +3,45 @@
   var modal_el = document.getElementById('contact-info-confirmation--modal');
 
   function clickSubmit(stepOneSubmitButton) {
-    stepOneSubmitButton[0].addEventListener("click",(event) => {
-      var phone_el = document.getElementsByName('0-contact_phone')[0]
-      var email_el = document.getElementsByName('0-contact_email')[0]
-      var requiredFieldSelected = document.getElementsByName('0-servicemember')[0].checked  ||  document.getElementsByName('0-servicemember')[1].checked
-      var noContactInfo = (phone_el.value && email_el.value)
+    stepOneSubmitButton[0].addEventListener('click', event => {
+      var phone_el = document.getElementsByName('0-contact_phone')[0];
+      var email_el = document.getElementsByName('0-contact_email')[0];
+      var requiredFieldSelected =
+        document.getElementsByName('0-servicemember')[0].checked ||
+        document.getElementsByName('0-servicemember')[1].checked;
+      var noContactInfo = phone_el.value && email_el.value;
 
       // If there is contact information OR if the required field is not filled out, no modal is needed
-      if ( noContactInfo || !requiredFieldSelected) {
+      if (noContactInfo || !requiredFieldSelected) {
         event.preventDefault();
-        submitNextButton.click()
-      }
-      else {
+        submitNextButton.click();
+      } else {
         event.preventDefault();
         var cancelModalButton = document.getElementById('external-link--cancel');
 
         // field_el is the field element that will need to be filled out.  We want to focus that field and scroll to it.
-        var field_el = {}
+        var field_el = {};
         if (noContactInfo || !email_el.value) {
-          field_el = email_el
-        }
-        else if (!phone_el.value) {
-          field_el = phone_el
+          field_el = email_el;
+        } else if (!phone_el.value) {
+          field_el = phone_el;
         }
         root.CRT.cancelModal(modal_el, cancelModalButton, field_el);
         root.CRT.openModal(modal_el);
       }
-    })
+    });
   }
 
   if (root.CRT.stageNumber === 1) {
-    var stepOneSubmitButton = document.getElementsByClassName('report-step-1-continue')
+    var stepOneSubmitButton = document.getElementsByClassName('report-step-1-continue');
     if (stepOneSubmitButton.length) {
-      var submitNextButton = document.getElementById('submit-next')
+      var submitNextButton = document.getElementById('submit-next');
       var continue_modal_button = document.getElementById('external-link--continue');
       continue_modal_button.onclick = function(event) {
         event.preventDefault();
         submitNextButton.click();
       };
-      clickSubmit(stepOneSubmitButton)
+      clickSubmit(stepOneSubmitButton);
     }
   }
-
 })(window);
