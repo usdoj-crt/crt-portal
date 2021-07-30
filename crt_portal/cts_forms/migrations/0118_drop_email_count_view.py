8da8ff4714277a447c3f2445c03a1419f7a04df9
lindsayyoung <lindsayn.young@gsa.gov>
2021-07-30 09:18:58 -0400

    complete removal of the old email tracking infrastructure

---
+++ crt_portal/cts_forms/migrations/0118_drop_email_count_view.py
@@ -0,0 +1,15 @@
+"""Removing unmanaged view created in migration #95"""
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cts_forms', '0117_swap_out_new_email_count'),
+    ]
+
+    operations = [
+        migrations.RunSQL("""
+            DROP VIEW email_report_count CASCADE;
+        """)
+    ]
