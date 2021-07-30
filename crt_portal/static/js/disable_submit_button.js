c6ec20f37e76ea3e677c0104134b41fcfb55ee71
Billy Hunt <billyhunt@navapbc.com>
2021-07-30 16:50:13 -0400

    Repond to code review

--- crt_portal/static/js/disable_submit_button.js
+++ crt_portal/static/js/disable_submit_button.js
@@ -2,7 +2,7 @@
   document.getElementById('report-form').addEventListener('submit', disableSubmitButton);
   function disableSubmitButton() {
     const submitNextButton = document.getElementById('submit-next');
-    const submitButton = document.getElementById('submit-next');
+    const submitButton = document.getElementById('submit');
     if (submitNextButton) submitNextButton.disabled = true;
     if (submitButton) submitButton.disabled = true;
   }
