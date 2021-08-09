56b91f541cc306e714661d8a0b7addc334f12024
Billy Hunt <billyhunt@navapbc.com>
2021-08-09 10:09:10 -0400

    Add disable to main report submit button.

--- crt_portal/static/js/disable_submit_button.js
+++ crt_portal/static/js/disable_submit_button.js
@@ -2,8 +2,10 @@
   document.getElementById('report-form').addEventListener('submit', disableSubmitButton);
   function disableSubmitButton() {
     const submitNextButton = document.getElementById('submit-next');
+    const submitNextTopButton = document.getElementById('submit-next-top');
     const submitButton = document.getElementById('submit');
     if (submitNextButton) submitNextButton.disabled = true;
+    if (submitNextTopButton) submitNextTopButton.disabled = true;
     if (submitButton) submitButton.disabled = true;
   }
 })(window);
