a36f0f3584778dcc837c74c55dfb52126e4b2e25
Billy Hunt <billyhunt@navapbc.com>
2021-09-20 15:17:25 -0400

    Remove print statements

--- crt_portal/cts_forms/utils.py
+++ crt_portal/cts_forms/utils.py
@@ -2,12 +2,8 @@ from .models import Report
 
 
 def update_email_count(email):
-    print('update_email_count')
-    print('email => ', email)
     if email is not None:
         reports = Report.objects.filter(contact_email__iexact=email)
-        print("report.email => ", [report.contact_email for report in reports])
         email_count = len(reports)
-        print("email_count => ", email_count)
         reports.update(email_count=email_count)
         return email_count
