a6fa1f24a2d7365cc2a8f3ad3aea3c7d7369beb9
Lou Huang <louhuang@navapbc.com>
2021-10-26 10:20:29 -0700

    feat(email): add webhook admin page

--- crt_portal/tms/urls.py
+++ crt_portal/tms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UnsubscribeView, WebhookView
+from .views import UnsubscribeView, WebhookView, AdminView
 
 app_name = 'tms'
 
 urlpatterns = [
     path('unsubscribe/<uuid:pk>/', UnsubscribeView.as_view(), name='email-unsubscribe'),
     path('webhook/', WebhookView.as_view(), name='tms-webhook'),
+    path('admin/', AdminView.as_view(), name='tms-admin'),
 ]
