2fa377172c972a7aa8483b6bc2a89b15e3ddca6f
Billy Hunt <billy@billyhunt.com>
2021-11-19 13:56:00 -0500

    Update DRS body information to add additional line and remove salutation.

---
+++ crt_portal/cts_forms/migrations/0130_drs_hhs_referral_letter_update.py
@@ -0,0 +1,47 @@
+from django.db import migrations
+
+
+def modify_drs_hhs_referral_letter(apps):
+    ResponseTemplate = apps.get_model('cts_forms', 'ResponseTemplate')
+    hhs_form_letter = ResponseTemplate.objects.get(title='DRS - HHS Referral Form Letter')
+    hhs_form_letter.body="""
+Re:		Civil Rights Division Complaint – {{ record_locator }} from the Disability Rights Section
+
+Thank you for contacting the Department of Justice on {{ date_of_intake }}.  We have reviewed the information you provided and have determined that the complaint raises issues that are more appropriately addressed by another federal agency.  We are, therefore, referring this complaint to the following agency for further action:
+
+Department of Health and Human Services
+1-800-368-1019
+
+What you can do:
+
+The above agency will review your complaint.  While we will take no further action on this matter, you can contact the agency above to check the status of your complaint.
+
+How you have helped:
+
+Although we are unable to act on your complaint, your report can help us find issues affecting multiple people or communities. It also helps us understand emerging trends and topics.
+
+Thank you for taking the time to contact the Department of Justice about your concerns.  
+
+Sincerely,
+
+
+U.S. Department of Justice
+Civil Rights Division
+"""
+
+    hhs_form_letter.save()
+
+def remove_drs_hhs_referral_letter_2(apps):
+    ResponseTemplate = apps.get_model('cts_forms', 'ResponseTemplate')
+    templates = ResponseTemplate.objects.filter(title__icontains='DRS - HHS Referral Form Letter')
+    templates.delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('cts_forms', '0129_drs_hhs_referral_letter'),
+    ]
+
+    operations = [
+        migrations.RunPython(modify_drs_hhs_referral_letter, remove_drs_hhs_referral_letter_2)
+    ]
