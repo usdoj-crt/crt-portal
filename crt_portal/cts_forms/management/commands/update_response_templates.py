3a72ae1b190d3c3ec032d65be142c7663e12a55c
Lou Huang <lou@louhuang.com>
2021-12-02 14:20:38 -0500

    feat(response templates): add ignore property and error handling

--- crt_portal/cts_forms/management/commands/update_response_templates.py
+++ crt_portal/cts_forms/management/commands/update_response_templates.py
@@ -1,5 +1,6 @@
 import os
 import frontmatter
+import yaml
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from cts_forms.models import ResponseTemplate
@@ -15,17 +16,35 @@ class Command(BaseCommand):
         for template in templates:
             if template.is_file() and template.name.endswith('.md'):
                 with open(template, 'r') as f:
-                    content = frontmatter.load(f)
+                    try:
+                        content = frontmatter.load(f)
+                    except yaml.scanner.ScannerError:
+                        self.stdout.write(self.style.ERROR(f'Response template {template.name} front-matter could not be parsed. Skipping it!'))
+                        continue
 
-                    letter_id = content['title']
-                    letter_data = {
-                        'subject': content['subject'],
-                        'language': content['language'],
-                        'body': content,
-                    }
+                    try:
+                        letter_id = content['title']
+                    except KeyError:
+                        self.stdout.write(self.style.ERROR(f'Response template {template.name} is missing required `title` property. Skipping it!'))
+                        continue
+
+                    if content.get('ignore', False):
+                        self.stdout.write(self.style.SUCCESS(f'Ignoring response template: {letter_id}'))
+                        continue
+
+                    try:
+                        letter_data = {
+                            'subject': content['subject'],
+                            'language': content['language'],
+                            'body': content,
+                        }
+                    except KeyError as e:
+                        self.stdout.write(self.style.ERROR(f'Response template {template.name} is missing required `{e.args[0]}` property. Skipping it!'))
+                        continue
+
                     letter, created = ResponseTemplate.objects.update_or_create(title=letter_id, defaults=letter_data)
 
                     if created:
-                        self.stdout.write(self.style.SUCCESS(f'Created response template: {letter_id}'))
+                        self.stdout.write(self.style.SUCCESS(f'Created response template: {letter.title}'))
                     else:
-                        self.stdout.write(self.style.SUCCESS(f'Updated response template: {letter_id}'))
+                        self.stdout.write(self.style.SUCCESS(f'Updated response template: {letter.title}'))
