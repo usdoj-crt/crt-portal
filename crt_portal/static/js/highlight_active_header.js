dcc1c401dca5a8bad5704ce1076a543c06027e66
Eileen McFarland <eileenmcfarland@navapbc.com>
2022-03-08 13:38:45 -0500

    make navbar sticky and highlight active heading

---
+++ crt_portal/static/js/highlight_active_header.js
@@ -0,0 +1,12 @@
+var toc = document.getElementById("toc");
+if (toc) {
+  var spy = new Gumshoe("#toc a", {
+    nested: true,
+    nestedClass: "active-parent"
+  });
+
+  toc.addEventListener("click", function(event) {
+    if (event.target.tagName !== "A") return;
+    expandPanel(event.target.hash);
+  });
+}
