e2e0dfafda9f8ac57f5ba06a47f95546bf62cbbe
Eileen McFarland <eileenmcfarland@navapbc.com>
2022-03-25 09:54:43 -0400

    update referrer script

--- crt_portal/static/js/set_referrer.js
+++ crt_portal/static/js/set_referrer.js
@@ -1,16 +1,16 @@
 (function(root) {
   referrer = document.referrer;
-  host = document.location.host
+  host = document.location.host;
   if (referrer.indexOf(host) > -1) {
-    return
+    return;
   }
 
   if (localStorage.getItem('referrer') == null && referrer != '') {
-    localStorage.setItem('referrer', referrer)
+    localStorage.setItem('referrer', referrer);
   }
 
   if (localStorage.getItem('referrer') != null && localStorage.getItem('referrer') != '') {
-    referrerField = document.findElementById("id_0-referrer")
-    referrerField.value = referrer
+    referrerField = document.findElementById("id_0-referrer");
+    referrerField.value = referrer;
   }
 })(window);
