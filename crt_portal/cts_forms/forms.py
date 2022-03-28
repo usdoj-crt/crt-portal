543ae8bd8d0eb9854f327402e2b6afa734bdeacf
Eileen McFarland <eileenmcfarland@navapbc.com>
2022-03-28 10:09:37 -0400

    persist referrer to db

--- crt_portal/cts_forms/forms.py
+++ crt_portal/cts_forms/forms.py
@@ -171,7 +171,6 @@ class Contact(ModelForm):
     def __init__(self, *args, **kwargs):
         ModelForm.__init__(self, *args, **kwargs)
         self.label_suffix = ''
-        self.fields['referrer'].initial = None
 
         self.fields['contact_first_name'].label = CONTACT_QUESTIONS['contact_first_name']
         self.fields['contact_last_name'].label = CONTACT_QUESTIONS['contact_last_name']
@@ -1727,6 +1726,7 @@ class ContactEditForm(ModelForm, ActivityStreamUpdater):
             }),
             'referrer': TextInput(attrs={
                 'class': 'usa-input',
+                'type': 'hidden'
             }),
         }
 
@@ -1736,8 +1736,6 @@ class ContactEditForm(ModelForm, ActivityStreamUpdater):
         self.fields['contact_phone'].error_messages = {'invalid': CONTACT_PHONE_INVALID_MESSAGE}
 
     def success_message(self):
-        # see if referrer available here
-        breakpoint()
         return self.SUCCESS_MESSAGE
 
 
