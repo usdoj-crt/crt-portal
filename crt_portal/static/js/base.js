2d466de487ed2de447748a5e9eeec9f3b0bd8ee8
Billy Hunt <billyhunt@navapbc.com>
2022-04-20 14:08:33 -0400

    Add beginnings of webpack config, specifically javascript to start, and the beginnings of css/scss

---
+++ crt_portal/static/js/base.js
@@ -0,0 +1,6 @@
+import 'vendor/uswds-init.min.js';
+import 'url_params_polyfill.min.js';
+import 'focus_alert.min.js';
+import 'clear_error_class.min.js';
+import 'banner_language_selection.min.js';
+import 'url_params_polyfill.min.js';
