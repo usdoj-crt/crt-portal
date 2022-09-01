d3897b2d48d05c71f37ccb958af8830b2a2574af
Billy Hunt <billyhunt@navapbc.com>
2022-09-01 10:56:20 -0400

    Add voting mode function.

---
+++ crt_portal/utils/voting_mode.py
@@ -0,0 +1,10 @@
+from cts_forms.models import VotingMode
+
+def is_voting_mode():
+    try:
+        voting_mode = VotingMode.objects.first()
+        return voting_mode.toggle
+    except AttributeError:
+        VotingMode.objects.create(toggle=False)
+        return False
+
