af6c020ed58a313a03f9934962b56aa94cf256a3
Billy Hunt <billyhunt@navapbc.com>
2022-09-02 10:44:21 -0400

    Add environment variable to turn on voting mode

--- crt_portal/utils/voting_mode.py
+++ crt_portal/utils/voting_mode.py
@@ -1,10 +1,12 @@
-from cts_forms.models import VotingMode
+# from cts_forms.models import VotingMode
+from django.conf import settings
 
 
 def is_voting_mode():
-    try:
-        voting_mode = VotingMode.objects.first()
-        return voting_mode.toggle
-    except AttributeError:
-        VotingMode.objects.create(toggle=False)
-        return False
+    return settings.VOTING_MODE
+    # try:
+    #     voting_mode = VotingMode.objects.first()
+    #     return voting_mode.toggle
+    # except AttributeError:
+    #     VotingMode.objects.create(toggle=False)
+    #     return False
