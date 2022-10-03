7fa72f8f49e2075ec999f3c61cfc10b8ebc2b83a
Billy Hunt <billyhunt@navapbc.com>
2022-10-03 10:16:17 -0400

    fix flake8 errors

--- crt_portal/cts_forms/management/commands/generate_repeat_writer_info.py
+++ crt_portal/cts_forms/management/commands/generate_repeat_writer_info.py
@@ -1,7 +1,8 @@
 import time
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from cts_forms.models import Report, RepeatWriterInfo
 from django.db import connection
+
 
 class Command(BaseCommand):
     help = "Generates reports for each year of activity, stored in comma separated value format (.csv)."
