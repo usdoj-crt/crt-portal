7505af9d2db6daa5769a3dcc8640369b6b720017
JackRyan1989 <jackryan@navapbc.com>
2022-12-08 15:25:53 -0500

    Added new expand all button

---
+++ crt_portal/static/js/dashboard_view_all.js
@@ -0,0 +1,40 @@
+(function(root, dom) {
+  var toggleAll = dom.querySelector('a.td-toggle-all');
+  toggleAll.onclick = function(event) {
+    var toggles = dom.querySelectorAll('a.td-toggle');
+    var arrow = toggleAll.children[0];
+    arrow.classList.contains('rotate') ? arrow.classList.remove('rotate') : arrow.classList.add('rotate');
+    for (var i = 0; i < toggles.length; i++) {
+      var image = toggles[i].children[0];
+      var id = toggles[i].dataset['id'];
+      var summary = dom.getElementById(`tr-additional-${id}`);
+      if (image.classList.contains('rotate')) {
+        image.classList.remove('rotate');
+        summary.setAttribute('hidden', '');
+      } else {
+        image.classList.add('rotate');
+        summary.removeAttribute('hidden');
+        // There's no "then" handler since the interaction is a quiet one.
+        // Use the network inspector to check on request and response content
+        if (toggleAll.dataset['posted'] !== 'true') {
+            window
+          .fetch(`/api/reports/${id}/`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRFToken': Cookies.get('csrftoken')
+            },
+            mode: 'same-origin',
+            body: JSON.stringify({ viewed: true })
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+        }
+    }
+    // Set a flag on the toggle so we don't resubmit data multiple times per session.
+    toggleAll.dataset['posted'] = 'true'
+    event.preventDefault();
+  };
+})(window, document);
