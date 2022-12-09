e22e3e0bcc87f98077c28ba74ac7b9bf9b010a29
JackRyan1989 <jackryan@navapbc.com>
2022-12-09 16:16:39 -0500

    Matching JS formatting style

--- crt_portal/static/js/dashboard_view_all.js
+++ crt_portal/static/js/dashboard_view_all.js
@@ -3,7 +3,9 @@
   toggleAll.onclick = function(event) {
     var toggles = dom.querySelectorAll('a.td-toggle');
     var arrow = toggleAll.children[0];
-    arrow.classList.contains('rotate') ? arrow.classList.remove('rotate') : arrow.classList.add('rotate');
+    arrow.classList.contains('rotate')
+      ? arrow.classList.remove('rotate')
+      : arrow.classList.add('rotate');
     for (var i = 0; i < toggles.length; i++) {
       var image = toggles[i].children[0];
       var id = toggles[i].dataset['id'];
@@ -17,24 +19,24 @@
         // There's no "then" handler since the interaction is a quiet one.
         // Use the network inspector to check on request and response content
         if (toggleAll.dataset['posted'] !== 'true') {
-            window
-          .fetch(`/api/reports/${id}/`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-CSRFToken': Cookies.get('csrftoken')
-            },
-            mode: 'same-origin',
-            body: JSON.stringify({ viewed: true })
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
+          window
+            .fetch(`/api/reports/${id}/`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': Cookies.get('csrftoken')
+              },
+              mode: 'same-origin',
+              body: JSON.stringify({ viewed: true })
+            })
+            .catch(error => {
+              console.error(error);
+            });
         }
+      }
     }
     // Set a flag on the toggle so we don't resubmit data multiple times per session.
-    toggleAll.dataset['posted'] = 'true'
+    toggleAll.dataset['posted'] = 'true';
     event.preventDefault();
   };
 })(window, document);
