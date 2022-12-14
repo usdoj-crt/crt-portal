19271b8f0e99610181b8f3f530b4f66af21b3f49
Kevin Kays <okkays@pm.me>
2022-12-14 08:58:50 -0500

    Modify the toggleAll to use the bulk route.
    Also saw an opportunity to remove a bit of nesting.

--- crt_portal/static/js/dashboard_view_all.js
+++ crt_portal/static/js/dashboard_view_all.js
@@ -1,42 +1,52 @@
 (function(root, dom) {
-  var toggleAll = dom.querySelector('a.td-toggle-all');
+
+  function toggleTarget(toggle) {
+    const arrow = toggle.querySelector('.icon');
+    const id = toggle.dataset['id'];
+    const summary = dom.getElementById(`tr-additional-${id}`);
+    arrow.classList.toggle('rotate');
+    summary.toggleAttribute('hidden');
+  }
+
+  function isToggled(target) {
+    return target.querySelector('.icon').classList.contains('rotate');
+  }
+
+  function markAsViewed(ids) {
+    if (!ids.length) return;
+    window.onbeforeunload = function() {
+      return 'Report views have not yet saved, are you sure you want to leave?';
+    };
+    window
+      .fetch(`/api/reports/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': Cookies.get('csrftoken')
+        },
+        mode: 'same-origin',
+        body: JSON.stringify({ report_pks: ids })
+      })
+      .then(() => window.onbeforeunload = null)
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const toggleAll = dom.querySelector('a.td-toggle-all');
   toggleAll.onclick = function(event) {
-    var toggles = dom.querySelectorAll('a.td-toggle');
-    var arrow = toggleAll.children[0];
-    arrow.classList.contains('rotate')
-      ? arrow.classList.remove('rotate')
-      : arrow.classList.add('rotate');
-    for (var i = 0; i < toggles.length; i++) {
-      var image = toggles[i].children[0];
-      var id = toggles[i].dataset['id'];
-      var summary = dom.getElementById(`tr-additional-${id}`);
-      if (image.classList.contains('rotate')) {
-        image.classList.remove('rotate');
-        summary.setAttribute('hidden', '');
-      } else {
-        image.classList.add('rotate');
-        summary.removeAttribute('hidden');
-        // There's no "then" handler since the interaction is a quiet one.
-        // Use the network inspector to check on request and response content
-        if (toggleAll.dataset['posted'] !== 'true') {
-          window
-            .fetch(`/api/reports/${id}/`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': Cookies.get('csrftoken')
-              },
-              mode: 'same-origin',
-              body: JSON.stringify({ viewed: true })
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        }
-      }
-    }
+    event.preventDefault();
+    const allToggled = isToggled(toggleAll);
+    toggleAll.querySelector('.icon').classList.toggle('rotate');
+    const toggles = [...dom.querySelectorAll('a.td-toggle')]
+      .filter(toggle => isToggled(toggle) == allToggled);
+
+    toggles.forEach(toggle => toggleTarget(toggle));
+
+    if (allToggled || toggleAll.dataset['posted'] === 'true') return;
+
+    markAsViewed(toggles.map(toggle => Number(toggle.dataset['id'])));
     // Set a flag on the toggle so we don't resubmit data multiple times per session.
     toggleAll.dataset['posted'] = 'true';
-    event.preventDefault();
   };
 })(window, document);
