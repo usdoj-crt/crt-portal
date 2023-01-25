313def61b7d1221fc2de2ef2be4184b1fe0b52ca
kewitham <katiewitham@navapbc.com>
2023-01-25 14:52:22 -0600

    address comments

--- urlchecker.sh
+++ urlchecker.sh
@@ -1,57 +1,58 @@
 #!/bin/bash
-# url testing
-# source urlchecker.sh
+# This script tests all urls in codebase (excluding the node_module directory) to catch any 404s
+# run source urlchecker.sh in the terminal
 main_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 excluded_directory="*/node_modules/*"
 all_urls=()
 all_files=()
-match_file()
+function print_files_for_url()
 {
+local url_to_match=$1
   for (( j=0; j<=${#all_urls[@]}; j++ )); do
-    if [ ${all_urls[$j]} = ${1} ]
+    if [ $all_urls[$j] = $url_to_match ]
       then
-        in_files+=("${all_files[$j]}")
-       fi
+        in_files+=($all_files[$j])
+    fi
     done
     echo "$in_files" >> output.csv
 }
 # add new headers file to get curl responses
-tmpheaderfile=$(mktemp headers)
+tmp_header_file=$(mktemp headers)
 # clear output file
 echo -n "" > output.csv
 # find links in all files in main directory except excluded directory 
-sudo find $main_dir -type f ! -path $excluded_directory -exec grep -iREo "(http|https)://[a-zA-Z0-9./?=_%:-]*" {} \; | while read url;
+find $main_dir -type f ! -path $excluded_directory -exec grep -iREo "(http|https)://[a-zA-Z0-9./?=_%:-]*" {} \; | while read url;
 # separate links from files and trim links
 do
-   all_files+=(${url})
+   all_files+=("$url")
    plain_url=${url#*:}
-   trimmed_url=$(echo "$plain_url" | sed 's:/*$::' | sed 's/\.$//')
-   all_urls+=(${trimmed_url})
+   trimmed_url=$(echo $plain_url | sed 's:/*$::' | sed 's/\.$//')
+   all_urls+=("$trimmed_url")
 done
 # check response for all unique urls
 unique_urls=($(tr ' ' '\n' <<<"${all_urls[@]}" | awk '!u[$0]++' | tr '\n' ' '))
-excluded_urls=("`cat $main_dir/excluded_urls`")
-for i in "${unique_urls[@]}"
+excluded_urls=("$(cat $main_dir/excluded_urls)")
+for i in ${unique_urls[@]}
 do
  # skip urls in excluded_urls file
- if echo ${excluded_urls[@]} | grep -q -w "${i}";
+ if echo ${excluded_urls[@]} | grep -q -w $i;
    then
         continue
  else
-    curl ${i} -I -o headers -s
+    curl $i -I -o headers -s
     response=$(cat headers | head -n 1 | cut '-d ' '-f2')
     in_files=()
-  if [ -z ${response} ];
+  if [ -z $response ];
     then
          continue
       
   fi
-  if [ ${response} -eq 404 ];
+  if [ $response -eq 404 ];
      then
     # if we get a 404 find matching index for file so we can put the url and the file where it was found in the csv
-    match_file $i
+    print_files_for_url $i
   fi
  fi
 done
 # remove headers file as it's no longer needed
-rm ${tmpheaderfile}
+rm $tmp_header_file
