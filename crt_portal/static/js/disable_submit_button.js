79b5309732c6cd98314373b5d8691337bd665bdd
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-02-17 11:39:53 -0500

    Disable the bottom submit button

--- crt_portal/static/js/disable_submit_button.js
+++ crt_portal/static/js/disable_submit_button.js
@@ -3,9 +3,11 @@
   function disableSubmitButton() {
     const submitNextButton = document.getElementById('submit-next');
     const submitNextTopButton = document.getElementById('submit-next-top');
+    const submitNextBottomButton = document.getElementById('submit-next-bottom');
     const submitButton = document.getElementById('submit');
     if (submitNextButton) submitNextButton.disabled = true;
     if (submitNextTopButton) submitNextTopButton.disabled = true;
+    if (submitNextBottomButton) submitNextBottomButton.disabled = true;
     if (submitButton) submitButton.disabled = true;
   }
 })(window);
