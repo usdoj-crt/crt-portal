9e58a693530e462a93d8af63cf455ef14b6b3061
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-02-17 13:14:41 -0500

    Disable the index migration so that dev can deploy

--- crt_portal/cts_forms/migrations/0164_create_actstream_description_index.py
+++ crt_portal/cts_forms/migrations/0164_create_actstream_description_index.py
@@ -1,13 +1,15 @@
 from django.db import migrations
-from django.db import connection
+
 
 def create_actstream_description_index(apps, schema_editor):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-        CREATE INDEX IF NOT EXISTS actstream_action_description_idx
-            ON public.actstream_action USING btree
-            (description)
-        """)
+    # with connection.cursor() as cursor:
+    #     cursor.execute("""
+    #     CREATE INDEX IF NOT EXISTS actstream_action_description_idx
+    #         ON public.actstream_action USING btree
+    #         (description)
+    #     """)
+    pass  # Noop - this breaks on dev due to:
+    # index row size 3008 exceeds btree version 4 maximum 2704 for index "actstream_action_description_idx"
 
 
 class Migration(migrations.Migration):
