5344ad8f43c9bd1cc810fc1d267934ad092fcf1f
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-05-08 15:50:41 -0400

    Create e2e tests that can log into the backend.
    - 🌎 Our e2e tests only touch the public-facing portion of the app.
    - ⛔ This leaves out our most complicated surfaces from testing.
    - ✅ This commit adds a way to authenticate / test backend processes.
    - 🔮 Future commits will add more tests to shore up backend functionality from regressions.

---
+++ crt_portal/cts_forms/management/commands/create_test_user.py
@@ -0,0 +1,28 @@
+import secrets
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):  # pragma: no cover
+    help = "Creates a mock user for testing."
+
+    def handle(self, *args, **options):
+        password = secrets.token_hex(32)
+        userhash = secrets.token_hex(32)
+        username = f'test-user-{userhash}'
+
+        # Clean out any existing test users to avoid conflicts:
+        User.objects.filter(username__startswith='test-user-').delete()
+
+        User.objects.create_superuser(
+            username=username,
+            email=f'{username}@example.com',
+            password=password
+        )
+
+        # Write the password to the current directory, so CI/CD can read it:
+        with open('test-user-username.txt', 'w') as f:
+            f.write(username)
+        with open('test-user-password.txt', 'w') as f:
+            f.write(password)
