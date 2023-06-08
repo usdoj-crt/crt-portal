9e6404a088c0d49423728effb4c71bab45c0579d
kewitham <katiewitham@navapbc.com>
2023-06-08 09:55:56 -0500

    lint

--- crt_portal/static/js/ga_util.js
+++ crt_portal/static/js/ga_util.js
@@ -20,38 +20,42 @@ function sendGAClickEvent(event_name) {
   function init() {
     const relatedReportButton = dom.getElementById('related-reports');
     if (relatedReportButton !== null) {
-      relatedReportButton.addEventListener('click', (e) => { sendGAClickEvent('view related reports') });
+      relatedReportButton.addEventListener('click', e => {
+        sendGAClickEvent('view related reports');
+      });
     }
-    const navItems = document.getElementsByClassName("usa-nav__primary-item");
+    const navItems = document.getElementsByClassName('usa-nav__primary-item');
     for (var i = 0; i < navItems.length; i++) {
-     const navItem = navItems[i];
-      navItem.addEventListener('click', (e) => sendGAPublicClickEvent('main nav ' + e.target.innerText))
-     }
-     const examples = document.getElementById("crt-landing--examples");
-     if (examples != null) {
-     const items = examples.getElementsByClassName("usa-nav__submenu-item");
-     for (let i = 0; i < items.length; i++) {
-       const item = items[i];
-       item.addEventListener('click', (e) => {
-         let target = e.target;
-         if (!target.dataset.hasOwnProperty("key")) {
-           // user clicked on the link; obtain the parent element.
-           target = e.target.parentElement;
-         }
-         const key = target.dataset["key"];
-         sendGAPublicClickEvent('example list ' + key);
-       });
-     }
+      const navItem = navItems[i];
+      navItem.addEventListener('click', e =>
+        sendGAPublicClickEvent('main nav ' + e.target.innerText)
+      );
     }
-     const infoButton = document.getElementById("info-link");
-     if (infoButton !== null) {
-      infoButton.addEventListener('click', sendGAPublicClickEvent('info button'))
+    const examples = document.getElementById('crt-landing--examples');
+    if (examples != null) {
+      const items = examples.getElementsByClassName('usa-nav__submenu-item');
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        item.addEventListener('click', e => {
+          let target = e.target;
+          if (!target.dataset.hasOwnProperty('key')) {
+            // user clicked on the link; obtain the parent element.
+            target = e.target.parentElement;
+          }
+          const key = target.dataset['key'];
+          sendGAPublicClickEvent('example list ' + key);
+        });
       }
-     const groupingButton = document.getElementById("grouping-select");
-     if (groupingButton !== null) {
-     groupingButton.addEventListener('change', (e) => {
-      sendGAClickEvent('grouping set to ' + e.target.value);
-     })
+    }
+    const infoButton = document.getElementById('info-link');
+    if (infoButton !== null) {
+      infoButton.addEventListener('click', sendGAPublicClickEvent('info button'));
+    }
+    const groupingButton = document.getElementById('grouping-select');
+    if (groupingButton !== null) {
+      groupingButton.addEventListener('change', e => {
+        sendGAClickEvent('grouping set to ' + e.target.value);
+      });
     }
   }
   window.addEventListener('DOMContentLoaded', init);
