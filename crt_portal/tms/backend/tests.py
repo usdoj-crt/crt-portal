bed3cf24248492ad66c02804733cd7905d61fb71
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-06-13 13:59:57 -0400

    Skip the failing TMS test

--- crt_portal/tms/backend/tests.py
+++ crt_portal/tms/backend/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import SimpleTestCase
 
 from ..api.client import TMSClient
@@ -7,6 +8,8 @@ from . import TMSEmailBackend
 class TMSEmailBackendTests(SimpleTestCase):
 
     def setUp(self):
+        if not hasattr(settings, 'TMS_TARGET_ENDPOINT'):
+            self.skipTest("TMS_TARGET_ENDPOINT not set")
         self.backend = TMSEmailBackend()
 
     def test_open_connection_is_a_tms_client(self):
