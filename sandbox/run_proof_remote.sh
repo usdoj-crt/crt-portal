c89b36458262eac299d59e0a71b1823588b6f865
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-06-16 15:57:51 -0400

    Update run_proof_remote.sh

--- sandbox/run_proof_remote.sh
+++ sandbox/run_proof_remote.sh
@@ -1,19 +1,5 @@
 PATH="$PATH:/home/vcap/deps/0/bin"
 
-if [[ "$VCAP_SERVICES" != "{}" ]]; then
-  echo -n 'Database name [via VCAP]: '
-  echo "$VCAP_SERVICES" | jq -r '."aws-rds"[].credentials.db_name'
-
-  echo -n 'Database username [via VCAP]: '
-  echo "$VCAP_SERVICES" | jq -r '."aws-rds"[].credentials.username'
-else
-  echo -n 'Database name [via env]: '
-  echo "$DB_NAME"
-
-  echo -n 'Database username [via env]: '
-  echo "$DB_USER"
-fi
-
 echo -n 'Can import dependencies? '
 echo "$(curl -s 'localhost:8080/try_to_import_prod')"
 
