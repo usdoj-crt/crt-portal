14114c3b206d82c0363840058e30fd3e1c1cbae0
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-06-28 11:32:01 -0400

    Fix a11y issue

--- crt_portal/cts_forms/tests/integration/education.py
+++ crt_portal/cts_forms/tests/integration/education.py
@@ -11,12 +11,3 @@ def test_education_accordion(page):
 
     assert page.locator('p').filter(has_text="Attacks, threats of violence, or destruction of property at place of worship").is_visible()
     assert page.locator('a').filter(has_text="Get help for hate crimes").is_visible()
-
-
-@pytest.mark.only_browser("chromium")
-def test_education_accordion_footer(page):
-    page.goto('/')
-
-    page.locator('.crt-landing--example_option').filter(has_text="Voting rights or ability to vote affected").click()
-
-    assert page.locator('#crt-landing-voting--example').is_visible()
