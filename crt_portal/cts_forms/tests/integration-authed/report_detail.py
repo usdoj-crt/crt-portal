dcc43f87757e7630c5bee653db6690521d1b210c
kewitham <katiewitham@navapbc.com>
2023-06-30 12:59:57 -0500

    update test

--- crt_portal/cts_forms/tests/integration-authed/report_detail.py
+++ crt_portal/cts_forms/tests/integration-authed/report_detail.py
@@ -20,17 +20,3 @@ def test_click_back_to_all(page):
         page.evaluate("document.getElementById('backtoall').click()")
 
     assert page.is_visible("#contact-email-filter")
-
-    page.goto("/form/dashboard/activity")
-
-    page.evaluate("document.querySelector('select[name=\"assigned_to\"]')[2].click()")
-    
-    with page.expect_navigation():
-        assert page.evaluate("document.querySelector('.td-link')[0].click()")
-    
-    assert page.is_visible("#contact-info")
-
-    with page.expect_navigation():
-        page.evaluate("document.getElementById('backtoall').click()")
-
-    assert page.is_visible("#actions")
