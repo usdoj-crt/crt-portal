79734613ebfe0ff8350cce1d2dd592d3e9fe09d2
kewitham <katiewitham@navapbc.com>
2023-11-16 13:58:26 -0600

    update forms

---
+++ crt_portal/static/js/mathjax-local.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+                TeX: {
+                    equationNumbers: {
+                    autoNumber: "AMS",
+                    useLabelIds: true
+                    }
+                },
+                tex2jax: {
+                    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                    displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+                    processEscapes: true,
+                    processEnvironments: true
+                },
+                displayAlign: 'center',
+                CommonHTML: {
+                    linebreaks: {
+                    automatic: true
+                    }
+                }
+            };
