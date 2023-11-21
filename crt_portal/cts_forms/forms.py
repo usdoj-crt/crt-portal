dd32a5483988dbead5e5754a04922ee90dc0d9da
kewitham <katiewitham@navapbc.com>
2023-11-21 10:23:26 -0600

    fix lints

--- crt_portal/cts_forms/forms.py
+++ crt_portal/cts_forms/forms.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 
 from django.contrib.auth import get_user_model
 from django.core.validators import ValidationError
