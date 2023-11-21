db1166fec57d9af7fb577af09ac7c84d9edf9a4f
kewitham <katiewitham@navapbc.com>
2023-11-21 10:59:48 -0600

    add messages

--- crt_portal/utils/activity.py
+++ crt_portal/utils/activity.py
@@ -2,6 +2,7 @@ import logging
 from actstream import action
 from cts_forms.models import Report
 from cts_forms.mail import notify
+from django.contrib import messages
 
 
 def send_action(user, *, verb, description, target, send_notification=False):
@@ -30,12 +31,15 @@ def _handle_notify_assigned_to(*, user, verb, description, target):
         logging.info(f'Not notifying assignee (no assignee) (report {report.id})')
         return
     if not hasattr(report.assigned_to, 'notification_preference'):
+        messages.add_message()
         logging.info(f'Not notifying assignee (no notification preference) (report {report.id})')
         return
     if not report.assigned_to.notification_preference.assigned_to:
+        messages.add_message()
         logging.info(f'Not notifying assignee (opted out of notification) (report {report.id})')
         return
     if not report.assigned_to.email:
+        messages.add_message()
         logging.warning(f'Not notifying assignee (User {report.assigned_to.id} is opted in, but has no email address)')
         return
     notify(template_title='assigned_to',
