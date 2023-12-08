f654f8f03e33fd357dc54da8d60c19fedbb53b75
Katie Witham <18104884+kewitham@users.noreply.github.com>
2023-12-08 11:21:04 -0600

    Update signals.py

--- crt_portal/cts_forms/signals.py
+++ crt_portal/cts_forms/signals.py
@@ -11,7 +11,7 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 
-from crt_portal.utils.request_utils import get_client_ip
+from utils.request_utils import get_client_ip
 
 from .models import Report, ProtectedClass, CommentAndSummary
 from .model_variables import PUBLIC_USER
