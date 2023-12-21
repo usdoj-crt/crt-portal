958ffd7414fce8ad16b00f6a7964cbfa345b7da2
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-12-21 13:41:41 -0500

    Apply a ten year label to all past records.
    - 🌎 Past records don't have retention schedules assigned
    - ⛔ They should receive a ten year label
    - ✅ This commit applies it via a migration
    
    The label will only be applied if:
    
    - The report is closed
    - There's not already a schedule
    - There's no lit hold
    - There's no DJ number

---
+++ crt_portal/cts_forms/tests/test_migrations.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+from django.apps import apps
+from cts_forms.models import RetentionSchedule, Report
+from cts_forms.tests.test_data import SAMPLE_REPORT_1
+
+import importlib
+migration = importlib.import_module('cts_forms.migrations.0189_backfill_ten_year_retention')
+
+
+class BackfillTenYearRetentionTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.ten_year_schedule = RetentionSchedule.objects.get(name='10 Year')
+        cls.not_ten_year_schedule = RetentionSchedule.objects.get(name='3 Year')
+
+    def setUp(self):
+        self.report = Report.objects.create(**SAMPLE_REPORT_1,
+                                            retention_schedule=None,
+                                            status='closed',
+                                            litigation_hold=False,
+                                            dj_number=None)
+
+    def test_backfills_successfully(self):
+        migration.backfill_ten_year_retention(apps, None)
+        self.report.refresh_from_db()
+
+        self.assertEqual(self.report.retention_schedule, self.ten_year_schedule)
+
+    def test_ignores_with_dj_number(self):
+        self.report.dj_number = '170-80-1234'
+        self.report.save()
+
+        migration.backfill_ten_year_retention(apps, None)
+        self.report.refresh_from_db()
+
+        self.assertIsNone(self.report.retention_schedule)
+
+    def test_ignores_open(self):
+        self.report.status = 'open'
+        self.report.save()
+
+        migration.backfill_ten_year_retention(apps, None)
+        self.report.refresh_from_db()
+
+        self.assertIsNone(self.report.retention_schedule)
+
+    def test_ignores_litigation_hold(self):
+        self.report.litigation_hold = True
+        self.report.save()
+
+        migration.backfill_ten_year_retention(apps, None)
+        self.report.refresh_from_db()
+
+        self.assertIsNone(self.report.retention_schedule)
+
+    def test_ignores_with_retention_schedule(self):
+        self.report.retention_schedule = self.not_ten_year_schedule
+        self.report.save()
+
+        migration.backfill_ten_year_retention(apps, None)
+        self.report.refresh_from_db()
+
+        self.assertEqual(self.report.retention_schedule, self.not_ten_year_schedule)
