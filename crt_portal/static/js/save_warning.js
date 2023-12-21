8de99fd860c05fc2fe1c4dbd6d438493c156b9a1
kewitham <katiewitham@navapbc.com>
2023-12-21 12:30:57 -0600

    Fix save warning

--- crt_portal/static/js/save_warning.js
+++ crt_portal/static/js/save_warning.js
@@ -1,9 +1,18 @@
 (function() {
   let showWarning = false;
-  const forms = document.querySelectorAll('.usa-form');
-  forms.forEach(form => {
-    form.addEventListener('change', () => (showWarning = true));
-  });
+  const commentForm = document.querySelector('#comment-actions-comment');
+  commentForm.addEventListener('change', () => (showWarning = true));
+  const actionForm = document.querySelector('#complaint-view-actions');
+  actionForm
+    .querySelectorAll('input')
+    .forEach(input => input.addEventListener('change', () => (showWarning = true)));
+  actionForm
+    .querySelectorAll('select')
+    .forEach(input => input.addEventListener('change', () => (showWarning = true)));
+  const contactForm = document.querySelector('#contact-edit-form');
+  contactForm.addEventListener('change', () => (showWarning = true));
+  const detailsForm = document.querySelector('#details-edit-form');
+  detailsForm.addEventListener('change', () => (showWarning = true));
   const btns = document.querySelectorAll('[type="submit"]');
   btns.forEach(btn => {
     btn.addEventListener('click', () => (showWarning = false));
