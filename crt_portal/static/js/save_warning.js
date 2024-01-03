fc1d5c2770b3b896aed912ee08530f735d7fd3c7
kewitham <katiewitham@navapbc.com>
2024-01-03 14:02:47 -0600

    update save warning logic for comboboxes

--- crt_portal/static/js/save_warning.js
+++ crt_portal/static/js/save_warning.js
@@ -1,18 +1,29 @@
 (function() {
   let showWarning = false;
+  const maybeAddWarning = function(e) {
+    if (
+      e.target.classList.contains('usa-combo-box__select') &&
+      !e.target.classList.contains('combobox-loaded')
+    ) {
+      e.target.classList.add('combobox-loaded');
+    } else {
+      showWarning = true;
+    }
+  };
+
   const commentForm = document.querySelector('#comment-actions-comment');
-  commentForm.addEventListener('change', () => (showWarning = true));
+  commentForm.addEventListener('change', maybeAddWarning);
   const actionForm = document.querySelector('#complaint-view-actions');
   actionForm
     .querySelectorAll('input')
-    .forEach(input => input.addEventListener('change', () => (showWarning = true)));
+    .forEach(input => input.addEventListener('change', maybeAddWarning));
   actionForm
     .querySelectorAll('select')
-    .forEach(input => input.addEventListener('change', () => (showWarning = true)));
+    .forEach(select => select.addEventListener('change', maybeAddWarning));
   const contactForm = document.querySelector('#contact-edit-form');
-  contactForm.addEventListener('change', () => (showWarning = true));
+  contactForm.addEventListener('change', maybeAddWarning);
   const detailsForm = document.querySelector('#details-edit-form');
-  detailsForm.addEventListener('change', () => (showWarning = true));
+  detailsForm.addEventListener('change', maybeAddWarning);
   const btns = document.querySelectorAll('[type="submit"]');
   btns.forEach(btn => {
     btn.addEventListener('click', () => (showWarning = false));
