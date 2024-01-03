245376471922fd9dbdee381e4e72fb4acb564d18
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-01-03 09:56:52 -0500

    Add an actstream entry for bulk 10 year

--- crt_portal/cts_forms/tests/test_migrations.py
+++ crt_portal/cts_forms/tests/test_migrations.py
@@ -20,11 +20,20 @@ class BackfillTenYearRetentionTest(TestCase):
                                             litigation_hold=False,
                                             dj_number=None)
 
+    def _get_migrate_activity(self):
+        return [
+            activity
+            for activity
+            in self.report.target_actions.all()
+            if (activity.verb == 'Retention schedule:' and activity.description == 'Migrated from "None" to "10 Year"')
+        ]
+
     def test_backfills_successfully(self):
         migration.backfill_ten_year_retention(apps, None)
         self.report.refresh_from_db()
 
         self.assertEqual(self.report.retention_schedule, self.ten_year_schedule)
+        self.assertEqual(len(self._get_migrate_activity()), 1)
 
     def test_ignores_with_dj_number(self):
         self.report.dj_number = '170-80-1234'
@@ -34,6 +43,7 @@ class BackfillTenYearRetentionTest(TestCase):
         self.report.refresh_from_db()
 
         self.assertIsNone(self.report.retention_schedule)
+        self.assertEqual(len(self._get_migrate_activity()), 0)
 
     def test_ignores_open(self):
         self.report.status = 'open'
@@ -43,6 +53,7 @@ class BackfillTenYearRetentionTest(TestCase):
         self.report.refresh_from_db()
 
         self.assertIsNone(self.report.retention_schedule)
+        self.assertEqual(len(self._get_migrate_activity()), 0)
 
     def test_ignores_litigation_hold(self):
         self.report.litigation_hold = True
@@ -52,6 +63,7 @@ class BackfillTenYearRetentionTest(TestCase):
         self.report.refresh_from_db()
 
         self.assertIsNone(self.report.retention_schedule)
+        self.assertEqual(len(self._get_migrate_activity()), 0)
 
     def test_ignores_with_retention_schedule(self):
         self.report.retention_schedule = self.not_ten_year_schedule
@@ -61,3 +73,4 @@ class BackfillTenYearRetentionTest(TestCase):
         self.report.refresh_from_db()
 
         self.assertEqual(self.report.retention_schedule, self.not_ten_year_schedule)
+        self.assertEqual(len(self._get_migrate_activity()), 0)
