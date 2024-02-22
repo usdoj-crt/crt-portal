813e605e94d97f448cffbfab89e9a0853073276d
Katie Witham <18104884+kewitham@users.noreply.github.com>
2024-02-22 13:40:35 -0800

    Update views.py

--- crt_portal/cts_forms/views.py
+++ crt_portal/cts_forms/views.py
@@ -1163,14 +1163,12 @@ class SavedSearchActionView(LoginRequiredMixin, View):
         section_args = get_section_args(section_filter)
         url = reverse('crt_forms:saved-searches')
         delete = request.POST.get('delete', False)
-        new_search = id == None
+        new_search = True if id else False
         if new_search:
             saved_search = SavedSearch()
             saved_search.created_by = request.user
         else:
             saved_search = get_object_or_404(SavedSearch, pk=id)
-
-
         form = SavedSearchActions(request.POST, instance=saved_search)
         if delete:
             saved_search.delete()
