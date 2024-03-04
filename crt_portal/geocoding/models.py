2c935a290b4208705be838da4f0c86309d1f8384
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-03-04 16:31:13 -0500

    Add models to support geocoding.
    - 🌎 We have csv's with lat/long data for counties
    - ⛔ We don't have a way to join our counties against that data
    - ✅ This commit adds database models for encoding county and state information from the census bureau
    - 🔮 Future commits will load data into these fields, and join it against judicial district data

---
+++ crt_portal/geocoding/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class State(models.Model):
+    """This model is used to store the state data from the US Census Bureau's TIGER/Line shapefiles
+
+    The fields are explicitly not renamed from their census bureau names to make it easier to migrate the format in the future.
+
+    See https://census.gov/cgi-bin/geo/shapefiles/index.php for more information.
+    """
+    name = models.CharField(max_length=255)
+    statefp = models.IntegerField()
+    stusps = models.CharField(max_length=2)
+    intptlat = models.FloatField()
+    intptlon = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class County(models.Model):
+    """This model is used to store the county data from the US Census Bureau's TIGER/Line shapefiles
+
+    The fields are explicitly not renamed from their census bureau names to make it easier to migrate the format in the future.
+
+    See https://census.gov/cgi-bin/geo/shapefiles/index.php for more information.
+    """
+    name = models.CharField(max_length=255)
+    statefp = models.IntegerField()
+    intptlat = models.FloatField()
+    intptlon = models.FloatField()
+
+    def __str__(self):
+        return self.name
