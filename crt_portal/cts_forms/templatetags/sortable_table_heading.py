e2a321a323d9effdf8d33ef163508d9324aa0182
kewitham <katiewitham@navapbc.com>
2024-03-13 10:16:30 -0500

    update batch table and model

--- crt_portal/cts_forms/templatetags/sortable_table_heading.py
+++ crt_portal/cts_forms/templatetags/sortable_table_heading.py
@@ -20,6 +20,9 @@ sort_lookup = {
     'retention schedule': 'retention_schedule',
     'closed date': 'closed_date',
     'expiration date': 'expiration_date',
+    'uuid': 'uuid',
+    'create date': 'create_date',
+    'proposed disposal date': 'proposed_disposal_date',
 }
 sortable_props = [
     'status',
@@ -38,6 +41,9 @@ sortable_props = [
     'retention_schedule',
     'closed_date',
     'expiration_date',
+    'uuid',
+    'create_date',
+    'proposed_disposal_date',
 ]
 
 
