d10555c46b461fdb540212e6771bb724fd16ce37
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-04-09 16:25:29 -0400

    Add a route for previewing saved search urls
    - 🌎 We want users to see the URL for their search as they type
    - ⛔ We'd have to reproduce our logic in javascript for that, and it wouldn't account for duplicate names
    - ✅ This commit adds an API route to expose saved search URL naming to the client
    - 🔮 Future commits will attach this to the client side create page

--- crt_portal/shortener/urls.py
+++ crt_portal/shortener/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import redirect_to_shortened
+from .views import redirect_to_shortened, preview_urlify
 
 app_name = 'shortener'
 
 
 urlpatterns = [
+    path(r'urlify/<path:prefix>', preview_urlify, name='urlify-preview-prefix'),
+    path(r'urlify/', preview_urlify, name='urlify-preview'),
     path(r'<path:path>', redirect_to_shortened, name='shortener-redirect')
 ]
--- crt_portal/shortener/views.py
+++ crt_portal/shortener/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.urls.exceptions import Http404
+from django.http import JsonResponse
 from .models import ShortenedURL
 
 
@@ -9,3 +11,11 @@ def redirect_to_shortened(request, *, path):
         return redirect(ShortenedURL.objects.get(pk=shortname))
     except ShortenedURL.DoesNotExist:
         raise Http404()
+
+
+@login_required
+def preview_urlify(request, *, prefix=''):
+    name = request.GET.get('name')
+    return JsonResponse({
+        'url': ShortenedURL.urlify(name, prefix=prefix)
+    })
