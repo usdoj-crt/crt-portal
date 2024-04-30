bb08111dcde7270ce44c925194be4f88ec6922aa
kewitham <katiewitham@navapbc.com>
2024-04-30 11:51:11 -0500

    lint and style tweaks

--- crt_portal/static/js/side_nav_slider.js
+++ crt_portal/static/js/side_nav_slider.js
@@ -1,14 +1,16 @@
 (function(root, dom) {
-    function toggleMenu () {
-        const sideNav = document.querySelector('.side-nav');
-        sideNav.classList.toggle('open');
-    }
-    function setUpSideNav () {
-        const mainWrapper = document.querySelector('.main-wrapper');
-        mainWrapper.classList.add('display-flex');
-        const menuSlider = mainWrapper.querySelector('.menu-slider');
-        menuSlider.addEventListener('click', toggleMenu)
-    }
+  const mainWrapperEls = document.querySelectorAll('.main-wrapper');
+  const mainWrapper = mainWrapperEls[mainWrapperEls.length - 1];
+  function toggleMenu() {
+    const sideNav = document.querySelector('.side-nav');
+    sideNav.classList.toggle('open');
+    mainWrapper.classList.toggle('side-nav-open');
+  }
+  function setUpSideNav() {
+    mainWrapper.classList.add('display-flex');
+    const menuSlider = mainWrapper.querySelector('.menu-slider');
+    menuSlider.addEventListener('click', toggleMenu);
+  }
 
   window.addEventListener('DOMContentLoaded', setUpSideNav);
 })(window, document);
