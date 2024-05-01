c9dcb91222c1f84dd6e7f312a79dc7d288c22f05
kewitham <katiewitham@navapbc.com>
2024-05-01 14:45:57 -0500

    front end logic for batch editing/rejection

---
+++ crt_portal/static/js/report_batch_controls.js
@@ -0,0 +1,29 @@
+(function(root, dom) {
+    const notesField = dom.querySelector('#id_notes');
+    function regexEscape(str) {
+        return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+    }
+    function updateBatchReports(e, reportBatchControl) {
+        e.preventDefault();
+        reportBatchControl.classList.toggle('removed');
+        reportBatchControl.closest('tr').classList.toggle('removed');
+        const id = reportBatchControl.getAttribute('id');
+        const note = id + ': [Reason]\n';
+        if (reportBatchControl.classList.contains('removed')) {
+            notesField.value += note;
+        } else {
+            const notesFieldVal = notesField.value;
+            const input = regexEscape(id);
+            const regex = new RegExp(input + '.*\\n');
+            const newVal = notesFieldVal.replace(regex, '');
+            notesField.value = newVal;
+        }
+    }
+    function listenForClick(reportBatchControl) {
+        reportBatchControl.addEventListener('click', (e) => updateBatchReports(e, reportBatchControl));
+      }
+    function attachListeners() {
+        dom.querySelectorAll('.report-batch-control').forEach(listenForClick);
+    }
+    root.addEventListener('load', attachListeners);
+})(window, document);
