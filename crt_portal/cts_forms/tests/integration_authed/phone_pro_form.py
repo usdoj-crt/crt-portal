0b945c19bc7a0c80eb0a6aa96c5e3b2a8892a34d
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-09-04 14:21:44 -0400

    Fix date on phone pro form test

--- crt_portal/cts_forms/tests/integration_authed/phone_pro_form.py
+++ crt_portal/cts_forms/tests/integration_authed/phone_pro_form.py
@@ -22,7 +22,7 @@ TEST_DATA = {
     "violation_summary": "something happened",
     "crt_reciept_month": '8',
     "crt_reciept_day": '15',
-    "crt_reciept_year": '2024',
+    "crt_reciept_year": '2023',
     "intake_format": "phone",
 }
 
@@ -45,6 +45,7 @@ def test_form(page, *, report):
         k: v for k, v in TEST_DATA.items() if k in ["contact_first_name", "contact_last_name"]
     })
 
+    page.locator('input#id_crt_receipt_year').fill('2023')
     page.locator('button[data-saves]').click()
     page.locator('.usa-alert').filter(has_text="Successfully created report").wait_for()
     report.screenshot(page, full_page=True, caption="One key feature of the phone intake form is that information can be partially filled in, saved, and then edited without leaving the page. We've only filled in the contact first and last name here before clicking save, and we're already presented with a Record Locator we can give to the caller.")
