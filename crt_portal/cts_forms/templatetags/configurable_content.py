000498a6e42a2dbe4d4b11ef874ea341e543cc88
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-10-11 15:16:55 -0400

    Fix nosec

--- crt_portal/cts_forms/templatetags/configurable_content.py
+++ crt_portal/cts_forms/templatetags/configurable_content.py
@@ -11,5 +11,5 @@ def configurable_content(machine_name):
 
     content = ConfigurableContent.objects.filter(machine_name=machine_name).first()
     if not content:
-        return mark_safe('Missing content for {}'.format(machine_name))
+        return mark_safe('Missing content for {}'.format(machine_name))  # nosec
     return mark_safe(content.render())  # nosec
