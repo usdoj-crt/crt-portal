9240a1c5bfbe7cf6ffd8e57a03653345486aac31
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-10-28 12:01:52 -0400

    Fix the current date on the phone pro form
    - 🌎 The phone pro form autopopulates the current date
    - ⛔ That date is calculated when the app instance starts, meaning that
      if the app instance runs a for a few days, the "today" date becomes
      invalid
    - ✅ This commit calculates the date on the frontend, instead, so it's
      always accurate to the client

--- crt_portal/static/js/autofill_current_date.js
+++ crt_portal/static/js/autofill_current_date.js
@@ -1,27 +1,33 @@
 (function(root, dom) {
   root.CRT = root.CRT || {};
 
-  function autofillTodaysDate(event) {
-    var inputEL = event.target.parentElement.getElementsByTagName('input');
-    var today = new Date();
-    var day = today.getDate(),
+  function autofillTodaysDate(inputEls) {
+    const today = new Date();
+    const day = today.getDate(),
       month = today.getMonth() + 1,
       year = today.getFullYear();
-    for (i = 0; i < inputEL.length; i++) {
-      if (inputEL[i].getAttribute('name').includes('month')) {
-        inputEL[i].value = month;
-      } else if (inputEL[i].getAttribute('name').includes('day')) {
-        inputEL[i].value = day;
-      } else if (inputEL[i].getAttribute('name').includes('year')) {
-        inputEL[i].value = year;
+    [...inputEls].forEach(inputEl => {
+      const inputName = inputEl.getAttribute('name');
+      if (inputName.includes('month')) {
+        inputEl.value = month;
+      } else if (inputName.includes('day')) {
+        inputEl.value = day;
+      } else if (inputName.includes('year')) {
+        inputEl.value = year;
       }
-    }
+    });
   }
 
-  var autofill_btns = document.getElementsByClassName('autofill_today_btn');
-  for (i = 0; i < autofill_btns.length; i++) {
-    autofill_btns[i].addEventListener('click', autofillTodaysDate);
-  }
+  document.querySelectorAll('.autofill_today_btn').forEach(autofillButton => {
+    autofillButton.addEventListener('click', event => {
+      const inputs = event.target.parentElement.getElementsByTagName('input');
+      autofillTodaysDate(inputs);
+    });
+  });
+
+  document.querySelectorAll('.autotoday').forEach(input => {
+    autofillTodaysDate([input]);
+  });
 
   return root;
 })(window, document);
