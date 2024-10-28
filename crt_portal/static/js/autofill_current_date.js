0a836000333fd108198490b813f3fc3578290c79
Kevin Kays <Kevin.Kays@usdoj.gov>
2024-10-28 13:05:44 -0400

    Fix tests for date field

--- crt_portal/static/js/autofill_current_date.js
+++ crt_portal/static/js/autofill_current_date.js
@@ -1,12 +1,13 @@
 (function(root, dom) {
   root.CRT = root.CRT || {};
 
-  function autofillTodaysDate(inputEls) {
+  function autofillTodaysDate(inputEls, overwrite) {
     const today = new Date();
     const day = today.getDate(),
       month = today.getMonth() + 1,
       year = today.getFullYear();
     [...inputEls].forEach(inputEl => {
+      if (!overwrite && inputEl.value) return;
       const inputName = inputEl.getAttribute('name');
       if (inputName.includes('month')) {
         inputEl.value = month;
@@ -21,12 +22,14 @@
   document.querySelectorAll('.autofill_today_btn').forEach(autofillButton => {
     autofillButton.addEventListener('click', event => {
       const inputs = event.target.parentElement.getElementsByTagName('input');
-      autofillTodaysDate(inputs);
+      autofillTodaysDate(inputs, true);
     });
   });
 
-  document.querySelectorAll('.autotoday').forEach(input => {
-    autofillTodaysDate([input]);
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.autotoday').forEach(input => {
+      autofillTodaysDate([input], false);
+    });
   });
 
   return root;
