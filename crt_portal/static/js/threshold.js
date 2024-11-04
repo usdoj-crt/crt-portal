6ce47e0dccecc9116da7eb6c6dd8511eb81decc2
kewitham <katiewitham@navapbc.com>
2024-11-04 14:17:16 -0600

    lint

--- crt_portal/static/js/threshold.js
+++ crt_portal/static/js/threshold.js
@@ -1,16 +1,15 @@
 (function() {
-    function showHideThresholdField(e) {
-      const wrapper = e.target.closest('.preference-wrapper');
-      const thresholdInput = wrapper.querySelector('input[value="threshold"]');
-      const thresholdField = wrapper.querySelector('.threshold');
-      thresholdField.querySelector('input').value = null;
-      thresholdField.hidden = !thresholdInput.checked;
-    }
+  function showHideThresholdField(e) {
+    const wrapper = e.target.closest('.preference-wrapper');
+    const thresholdInput = wrapper.querySelector('input[value="threshold"]');
+    const thresholdField = wrapper.querySelector('.threshold');
+    thresholdField.querySelector('input').value = null;
+    thresholdField.hidden = !thresholdInput.checked;
+  }
 
-    document.querySelectorAll('.usa-radio__input').forEach(radioInput => {
-    radioInput.addEventListener('change', (e) => {
-        showHideThresholdField(e);
+  document.querySelectorAll('.usa-radio__input').forEach(radioInput => {
+    radioInput.addEventListener('change', e => {
+      showHideThresholdField(e);
     });
-    });
-
-  })();
+  });
+})();
