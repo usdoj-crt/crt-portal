dee4b9e8ae07821dc6e6ae056b4ed504d4b8f0f8
Richard Wolfe <richardwolfe@navapbc.com>
2025-01-22 11:21:10 -0500

    fix instance where eeoc_office does not have assigned variables

--- crt_portal/cts_forms/models.py
+++ crt_portal/cts_forms/models.py
@@ -1239,13 +1239,14 @@ class ResponseTemplate(models.Model):
         if report.contact_email:
             contact_email = report.contact_email
 
+        eeoc_office = report.eeoc_office
         eeoc_office_name = ''
         eeoc_office_url = ''
-        if report.eeoc_office:
-            if report.eeoc_office.name:
-                eeoc_office_name = report.eeoc_office_name
-            if report.eeoc_office.url:
-                eeoc_office_url = report.eeoc_office.url
+        if eeoc_office:
+            if eeoc_office.name:
+                eeoc_office_name = eeoc_office.name
+            if eeoc_office.url:
+                eeoc_office_url = eeoc_office.url
 
         return Context({
             'record_locator': report.public_id,
