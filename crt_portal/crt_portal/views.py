f5d5f149a6fdc2c853dc473e2fecf65cd47f7371
Richard Wolfe <richardwolfe@navapbc.com>
2025-03-06 21:34:27 -0500

    add print statements for debugging

--- crt_portal/crt_portal/views.py
+++ crt_portal/crt_portal/views.py
@@ -17,11 +17,15 @@ def retrieve_and_save_next_url_in_session(request):
 
 @login_required
 def crt_loggedin_view(request):
+    print("CrtLogin: Logged In")
     next_page = request.session.get("next_page")
     if next_page:
+        print("CrtLogin: Next Page = ", next_page)
         next_url = reverse('crt_landing_page') + next_page
+        print("CrtLogin: Next Url = ", next_url)
         if url_has_allowed_host_and_scheme(next_url, None):
             safe_url = iri_to_uri(next_url)
+            print("CrtLogin: Safe Url = ", safe_url)
             return redirect(safe_url)
     return redirect('crt_landing_page')
 
