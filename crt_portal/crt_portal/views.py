17cc176e970c879c407a76d714e79bde47e2d1e1
Richard Wolfe <richardwolfe@navapbc.com>
2025-03-06 20:41:46 -0500

    cleanup code

--- crt_portal/crt_portal/views.py
+++ crt_portal/crt_portal/views.py
@@ -2,33 +2,23 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render
+from django.utils.http import is_safe_url
+from django.shortcuts import redirect
 
 
 def retrieve_and_save_next_url_in_session(request):
-    print("CrtViews: Saving Next Url...")
-
     next_url = request.GET.get('next', '/')
-    print("Next Url = ", next_url)
-
     request.session['next_page'] = next_url
-
-    print("Request Session = ", request.session.__dict__)
-    print("CrtViews: Next Url Saved.")
-
     request.session.modified = True
     request.session.save()
 
 
 @login_required
 def crt_loggedin_view(request):
-    print("CrtLoggedinView: LoggedIn")
-    print("Request Session = ", request.session.__dict__)
-    print("Request User = ", request.user)
-
-    print("next_url = ", request.session.get("next_page"))
-
-    return render(request, 'loggedin.html')
+    next_url = request.session.get("next_page")
+    if next_url and is_safe_url(next_url, allowed_hosts={request.get_host()}):
+        return redirect(next_url)
+    return redirect('crt_landing_page')
 
 
 class CrtLoginView(LoginView):
@@ -36,7 +26,6 @@ class CrtLoginView(LoginView):
     template_name = 'registration/login.html'
 
     def get(self, request, *args, **kwargs):
-        print("CrtLoginView: Get")
         self.save_next_url(request)
         return super(CrtLoginView, self).get(request, *args, **kwargs)
 
@@ -48,10 +37,8 @@ class CrtAdminLoginView(LoginView):
     template_name = 'admin/login.html'
 
     def get(self, request, *args, **kwargs):
-        print("CrtAdminLoginView: Get")
         self.save_next_url(request)
         return super(CrtAdminLoginView, self).get(request, *args, **kwargs)
 
     def save_next_url(self, request):
         retrieve_and_save_next_url_in_session(request)
-
