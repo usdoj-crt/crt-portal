66a829392697129ac6973ab4b15cd0b08377dc6f
Richard Wolfe <richardwolfe@navapbc.com>
2025-03-06 22:03:16 -0500

    try remove reverse lookup

--- crt_portal/crt_portal/views.py
+++ crt_portal/crt_portal/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.utils.encoding import iri_to_uri
 from django.shortcuts import redirect
-from django.urls import reverse
 
 
 def retrieve_and_save_next_url_in_session(request):
@@ -20,11 +19,9 @@ def crt_loggedin_view(request):
     print("CrtLogin: Logged In")
     next_page = request.session.get("next_page")
     if next_page:
-        print("CrtLogin: Next Page = ", next_page)
-        next_url = reverse('crt_landing_page') + next_page
-        print("CrtLogin: Next Url = ", next_url)
-        if url_has_allowed_host_and_scheme(next_url, None):
-            safe_url = iri_to_uri(next_url)
+        print("CrtLogin: Next Url = ", next_page)
+        if url_has_allowed_host_and_scheme(next_page, None):
+            safe_url = iri_to_uri(next_page)
             print("CrtLogin: Safe Url = ", safe_url)
             return redirect(safe_url)
     return redirect('crt_landing_page')
