26a38788369b689c1a9f76e1267e22c21e50416d
Richard Wolfe <richardwolfe@navapbc.com>
2025-04-28 13:26:47 -0400

    update decorator import

--- crt_portal/crt_portal/decorators.py
+++ crt_portal/crt_portal/decorators.py
@@ -5,6 +5,7 @@ def active_user_required(func):
     def wrapper(request, *args, **kwargs):
         print("Active User Required Decorator is called!")
         user = request.user
+        print("User = ", user)
         if not user.is_active:
             print("User is not active, redirecting to landing.")
             return redirect('/form/landing')
