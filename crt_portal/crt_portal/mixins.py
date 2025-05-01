cd3d75c9bc05bc0ae52f1ffc4334ee4c1e071223
Richard Wolfe <richardwolfe@navapbc.com>
2025-05-01 00:35:17 -0400

    update mixin for superusers

--- crt_portal/crt_portal/mixins.py
+++ crt_portal/crt_portal/mixins.py
@@ -5,6 +5,9 @@ class PortalAccessRequiredMixin():
     """Verify that the current user is has portal access."""
     def dispatch(self, request, *args, **kwargs):
         user = request.user
+        if user.is_superuser or user.is_staff:
+            return super().dispatch(request, *args, **kwargs)
+
         if not hasattr(user, 'profile'):
             return redirect('/form/landing')
 
