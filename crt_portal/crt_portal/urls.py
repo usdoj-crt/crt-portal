0f68ceaa3179e4e9174ee852a53fd07d504d5b6c
Richard Wolfe <richardwolfe@navapbc.com>
2025-05-20 16:58:05 -0400

    fix url for logout

--- crt_portal/crt_portal/urls.py
+++ crt_portal/crt_portal/urls.py
@@ -42,6 +42,7 @@ from django.views.generic import TemplateView
 from .views import crt_loggedin_view, crt_logout_view, crt_loggedout_view, CrtLoginView, CrtAdminLoginView
 
 environment = os.environ.get('ENV', 'UNDEFINED')
+auth = []
 if environment in ['PRODUCTION', 'STAGE']:
     from mozilla_django_oidc import views as oidc_views
     auth = [
@@ -56,11 +57,11 @@ if environment in ['PRODUCTION', 'STAGE']:
         path('admin/login/', CrtAdminLoginView.as_view(), name='admin_login'),
         path('crt-login/login/', CrtLoginView.as_view(), name='login'),
         path('crt-login/loggedin/', crt_loggedin_view, name='crt_logged_in'),
-        path('crt-login/logout/', crt_logout_view, name='crt_logout'),
         path('crt-login/loggedout/', crt_loggedout_view, name='crt_logged_out')
     ]
 else:
     auth = []
+auth.append(path('crt-login/logout/', crt_logout_view, name='crt_logout'))
 
 
 def redirect_static(request):
