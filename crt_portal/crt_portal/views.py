c4bf21445474813b950c59d4af21b263570eab99
Richard Wolfe <richardwolfe@navapbc.com>
2025-05-27 10:33:29 -0400

    autodeploy to stage for testing

--- crt_portal/crt_portal/views.py
+++ crt_portal/crt_portal/views.py
@@ -1,7 +1,7 @@
-# import os
-# import urllib.parse
+import os
+import urllib.parse
 
-# from django.conf import settings
+from django.conf import settings
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.utils.encoding import iri_to_uri
 from django.shortcuts import redirect, render
-# from django.middleware.csrf import get_token
+from django.middleware.csrf import get_token
 
 from .decorators import portal_access_required
 
@@ -22,19 +22,19 @@ def retrieve_and_save_next_url_in_session(request):
     request.session.save()
 
 
-# def handle_oidc_logout(id_token):
-#     base_uri = f'{settings.login_base_url}'
-#     logout_uri = f'{settings.LOGOUT_REDIRECT_URL}'
-#     logout_redirect_uri = base_uri + logout_uri
+def handle_oidc_logout(id_token):
+    base_uri = f'{settings.login_base_url}'
+    logout_uri = f'{settings.LOGOUT_REDIRECT_URL}'
+    logout_redirect_uri = base_uri + logout_uri
 
-#     url = f'{settings.OIDC_OP_LOGOUT_ENDPOINT}?'
-#     params = {
-#         'id_token_hint': id_token,
-#         'post_logout_redirect_uri': logout_redirect_uri
-#     }
-#     request = url + urllib.parse.urlencode(params)
-#     print("Handling oidc logout: Request URL =", request)
-#     return redirect(request)
+    url = f'{settings.OIDC_OP_LOGOUT_ENDPOINT}?'
+    params = {
+        'id_token_hint': id_token,
+        'post_logout_redirect_uri': logout_redirect_uri
+    }
+    request = url + urllib.parse.urlencode(params)
+    print("CrtLogout Debug: Logout Request URL =", request)
+    return redirect(request)
 
 
 @login_required
@@ -50,13 +50,13 @@ def crt_loggedin_view(request):
 
 
 def crt_logout_view(request):
-    # environment = os.environ.get('ENV', 'UNDEFINED')
-    # if environment in ['PRODUCTION', 'STAGE']:
-    #     id_token = request.session.get('oidc_id_token')
-    #     csrf_token = get_token(request)
-    #     print("CrtLogout Debug: Id Token =", id_token)
-    #     print("CrtLogout Debug: csrf_token =", csrf_token)
-    #     handle_oidc_logout(id_token)
+    environment = os.environ.get('ENV', 'UNDEFINED')
+    if environment in ['PRODUCTION', 'STAGE']:
+        id_token = request.session.get('oidc_id_token')
+        csrf_token = get_token(request)
+        print("CrtLogout Debug: Id Token =", id_token)
+        print("CrtLogout Debug: csrf_token =", csrf_token)
+        handle_oidc_logout(id_token)
     return redirect('logout')
 
 
